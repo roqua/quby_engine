1a3a244ed052a477d45ffc76668d4e7dc132ade4
Marten Veldthuis <marten@veldthuis.com>
2009-12-03 21:11:07 +0100

    move application.js to answers.js and do per-controller js files

--- public/javascripts/answers.js
+++ public/javascripts/answers.js
@@ -8,31 +8,37 @@ function activatePanel(panel) {
     panel.show().addClass('current');
 }
 
-$(document).ready(function() {
-    
-    // enable javascript-based previous/next links
-    $(".panel .prevnext").show();
-
-    // hide all panels except first one
-    $(".panel").hide();
-    $(".buttons").hide();
-    $(".panel:first").show().addClass('current');
-
-    // hide first previous button, and last next button
-    $(".panel:first .prevnext a.prev").hide();
-    $(".panel:last  .prevnext a.next").hide();
-
-    // show previous panel
-    $(".panel a.prev").click(function(event) {
-        activatePanel($(this).parents('.panel').prev());
-        return false;
-    });
-    
-    // show next panel
-    $(".panel a.next:not(:last)").click(function(event) {
-        activatePanel($(this).parents('.panel').next());
-        return false;
-    });
-
-})
+$(document).ready(
+    function() { 
+        
+        // enable javascript-based previous/next links
+        $(".panel .prevnext").show();
+        
+        // hide all panels except first one
+        $(".panel").hide();
+//        $(".buttons").hide();
+        $(".panel:first").show().addClass('current');
+        
+        // hide first previous button, and last next button
+        $(".panel:first .prevnext .prev").hide();
+        $(".panel:last  .prevnext .next").hide();
+        
+        // show previous panel
+        $(".panel .prev").click(
+            function(event) {
+                activatePanel($(this).parents('.panel').prev());
+                return false;
+            }
+        );
+        
+        // show next panel
+        $(".panel .next:not(:last)").click(
+            function(event) {
+                activatePanel($(this).parents('.panel').next());
+                return false;
+            }
+        );
+        
+    }
+);
 
