8ff8eb0f356962627d8e2c1919847d04f31527ac
Marten Veldthuis <marten@veldthuis.com>
2010-05-07 10:37:51 +0200

    Move reports. Clean up empty helpers

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,4 +1,4 @@
-class ReportsController < ApplicationController
+class Admin::ReportsController < AdminAreaConntroller
   def show
     q = Questionnaire.find(params[:questionnaire_id])
     @questionnaire = q['type'].constantize.find(q.id)
--- app/helpers/admin/functions_helper.rb
+++
@@ -1,2 +0,0 @@
-module Admin::FunctionsHelper
-end
