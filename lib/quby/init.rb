a9db92e305a8c81f934528e7351aeeebe037072f
Marten Veldthuis <marten@veldthuis.com>
2010-08-24 13:53:49 +0200

    remove old files

--- lib/quby/init.rb
+++
@@ -1,6 +0,0 @@
-QUESTIONNAIRES = []
-
-#load Rails.root.join('lib', 'quby', 'classes.rb')
-#load Rails.root.join('lib', 'quby', 'factories.rb')
-
-#load Rails.root.join('app', 'questionnaires', 'honosca.rb')
