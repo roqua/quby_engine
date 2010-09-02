d956adc60fc918ceedf1ce044e8f2ac95395779a
Marten Veldthuis <marten@veldthuis.com>
2010-09-02 12:40:49 +0200

    add jquery methods for implementing placeholders in older browsers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//
+
+
+// Fix HTML5 <input placeholder="foo"/> feature for older browsers
+
+$('[placeholder]').focus(function() {
+  var input = $(this);
+  if (input.val() == input.attr('placeholder')) {
+    input.val('');
+    input.removeClass('placeholder');
+  }
+}).blur(function() {
+  var input = $(this);
+  if (input.val() == '') {
+    input.addClass('placeholder');
+    input.val(input.attr('placeholder'));
+  }
+}).blur();
+
+$('[placeholder]').parents('form').submit(function() {
+  $(this).find('[placeholder]').each(function() {
+    var input = $(this);
+    if (input.val() == input.attr('placeholder')) {
+      input.val('');
+    }
+  })
+});
