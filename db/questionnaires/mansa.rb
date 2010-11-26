69e972793fd4ad9c4e85d278bdc83e108cdb611f
Ando Emerencia <ando.emerencia@gmail.com>
2010-11-26 23:28:09 +0100

    removed double mansa answer options

--- db/questionnaires/mansa.rb
+++ db/questionnaires/mansa.rb
@@ -10,73 +10,73 @@ description ""
 question :v_1, :type => :radio do
   title "1. Hoe tevreden bent u met uw leven als geheel?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_6, :type => :radio do
   title "2. Hoe tevreden bent u met uw woning?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_7, :type => :radio do
   title "3. Hoe tevreden bent u met uw huisgenoten of als u alleen woont, hoe tevreden bent u met het feit dat u alleen woont?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_8, :type => :radio do
   title "4. Hoe tevreden bent u met uw dagbesteding?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_9, :type => :radio do
   title "5. Hoe tevreden bent u met uw lichamelijke gezondheid?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_10, :type => :radio do
   title "6. Hoe tevreden bent u met uw psychische gezondheid?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_11, :type => :radio do
@@ -89,13 +89,13 @@ end
 question :v_12, :type => :radio do
   title "8. Hoe tevreden bent u met uw persoonlijke veiligheid?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_13, :type => :radio do
@@ -122,60 +122,60 @@ end
 question :v_16, :type => :radio do
   title "12. Hoe tevreden bent u over uw sociale relaties?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_17, :type => :radio do
   title "13. Hoe tevreden bent u met de relatie die u heeft met uw familie?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_18, :type => :radio do
   title "14. Hoe tevreden bent u met uw huidige relatie of met het feit dat u geen relatie heeft?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_19, :type => :radio do
   title "15. Hoe tevreden bent u met uw seksuele leven?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
 question :v_20, :type => :radio do
   title "16. Hoe tevreden bent u met uw financiële situatie?"
   description ""
-  option :a1, :value => 1, :description => "zeer ontevreden zeer ontevreden"
+  option :a1, :value => 1, :description => "zeer ontevreden"
   option :a2, :value => 2, :description => "ontevreden"
   option :a3, :value => 3, :description => "een beetje ontevreden"
   option :a4, :value => 4, :description => "gemengd"
   option :a5, :value => 5, :description => "een beetje tevreden"
   option :a6, :value => 6, :description => "tevreden"
-  option :a7, :value => 7, :description => "zeer tevreden zeer tevreden"
+  option :a7, :value => 7, :description => "zeer tevreden"
 end
 
