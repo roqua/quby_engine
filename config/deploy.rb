ba3c12209282b82dbb219c308066232e27e58ed6
Marten Veldthuis <marten@veldthuis.com>
2010-12-09 14:51:20 +0100

    dont write database yml on setup, we do that in the server setup script

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,34 +25,6 @@ namespace :deploy do
     # Do nothing
   end
   
-  desc "Create database.yml in shared/config" 
-  task :after_setup do
-    database_configuration = <<-EOF
-development:
-  adapter: sqlite3
-  database: db/development.sqlite3
-  timeout: 5000
-
-test:
-  adapter: sqlite3
-  database: db/test.sqlite3
-  timeout: 5000
-
-production:
-  adapter: mysql
-  database: quby_#{application}
-  username: quby_#{application}
-  password: roquargoc
-  encoding: utf8
-  host: localhost
-  reconnect: true
-EOF
-
-  run "mkdir -p #{deploy_to}/#{shared_dir}/config" 
-  put database_configuration, "#{deploy_to}/#{shared_dir}/config/database.yml" 
-end
-
-
   desc "Link in the production database.yml" 
   task :after_update_code do
     run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
