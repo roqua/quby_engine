eb3e9be204f4295f1a9a962ec8d25edaeb9cd467
Marten Veldthuis <marten@veldthuis.com>
2010-12-21 12:36:17 +0100

    fix three-space indent

--- db/questionnaires/Voorbeeldvragenlijst.rb
+++ db/questionnaires/Voorbeeldvragenlijst.rb
@@ -47,9 +47,9 @@ ENDTEXT
   end
 
   question :q03, :type => :string do
-     title "Textveld"
-     #description: In questions kan je een description gebruiken om extra uitleg te geven bij een vraag
-     description "Dit is een tekstveld"
+    title "Textveld"
+    #description: In questions kan je een description gebruiken om extra uitleg te geven bij een vraag
+    description "Dit is een tekstveld"
   end
 end #Deze end hoort bij de eerste panel do
 #Het is handig om met inspringing aan te geven of je binnen een panel of een question blok zit of niet
