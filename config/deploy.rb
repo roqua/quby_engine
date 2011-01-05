ccd0b7b0a3a63407c7ac4bcb072d24efc060eed1
Marten Veldthuis <marten@veldthuis.com>
2011-01-05 15:54:39 +0100

    i think this should be a workable option for remote questionnaire merging

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :repository, "git@git.roqua.nl:quby.git"
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 
+set :questionnaire_repository, "git@git.roqua.nl:qubyquestionnaires.git"
+set :questionnaire_branch, "master"
+
 set :user, "deploy"
 set :use_sudo, false
 
@@ -29,5 +32,23 @@ namespace :deploy do
   desc "Link in the production database.yml" 
   task :after_update_code do
     run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
+  end
+
+  desc "Symlink the questionnaires from shared dir"
+  task :link_shared_dirs do
+    run "rm -r #{release_path}/db/questionnaires"
+    run "ln -nfs #{deploy_to}/#{shared_dir}/questionnaires #{release_path}/db/questionnaires"
+  end
+
+  desc "Create/update the questionnaires repo checkout"
+  task :update_questionnaires do
+    command = "if [ -d #{deploy_to}/#{shared_dir}/questionnaires ]; then " +
+                # repository already exists
+                "cd #{deploy_to}/#{shared_dir}/questionnaires && git pull;" + 
+              "else " + 
+                # repository does not exist yet, do a clone
+                "cd #{deploy_to}/#{shared_dir} && git clone #{questionnaire_repository} questionnaires; " + 
+              "fi"
+    run command
   end
 end
