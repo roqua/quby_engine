dbbc8c873869372ca5fd2f474268bd7d03f3f8d0
Marten Veldthuis <marten@veldthuis.com>
2011-01-05 13:45:59 +0100

    Whoops, that didn't actually work. This does.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -39,7 +39,7 @@ class AnswersController < ApplicationController
   end
 
   def edit
-    render :action => "#{session[:display_mode]}/edit"
+    render :action => "answers/#{session[:display_mode]}/edit"
   end
 
   def create
