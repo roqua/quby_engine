6b860ce4c906784d9a9c89084f6e337a1a771c6f
Jorn van de Beek <jornvandebeek@roqua.nl>
2011-01-12 15:09:02 +0100

    Fix merge oopsie.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -123,9 +123,10 @@ class Answer < ActiveRecord::Base
       end
       
       if (question.parent and (question.parent.type == :radio and value[question.parent.key] != question.parent_option_key.to_s) or
-        (question.parent.type == :check_box and value[question.parent.key][question.parent_option_key] == 0)) or
-        hidden_questions.include?(question.key) 
-        clear_question(question)
+          (question.parent.type == :check_box and value[question.parent.key][question.parent_option_key] == 0)) or
+          hidden_questions.include?(question.key) or
+          answer == "DESELECTED_RADIO_VALUE" 
+        clear_question(question) 
         next          
       end
       
