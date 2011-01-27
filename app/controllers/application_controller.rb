3cf0f4338b0aa4d554d3751882c536c39ed836cc
Marten Veldthuis <marten@veldthuis.com>
2011-01-27 17:09:49 +0100

    whoops, sorry ando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-
+require 'ipaddr'
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def ip_check_for_api_methods
     # SECURITY CRITICAL : Checks whether this API method call is coming from one of our own servers
-    return true if Rails.env.development?
+    #return true if Rails.env.development?
     return true if Settings.api_allowed_ip_ranges.blank?
 
     Settings.api_allowed_ip_ranges.find { |range_or_addr| IPAddr.new(range_or_addr).include?(IPAddr.new(request.remote_ip)) }
