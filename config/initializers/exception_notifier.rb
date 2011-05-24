5cc97b4730a6664c1e2013b9e097ad2ad518fd70
Marten Veldthuis <marten@veldthuis.com>
2011-05-24 14:03:15 +0200

    add conditional to exception_notifier initializer to prevent endless recursion in Settings

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -2,5 +2,8 @@ unless Rails.env.development?
   Quby::Application.config.middleware.use ExceptionNotifier,
    :email_prefix => "[Quby] ",
    :sender_address => %{"Exception Notifier" <noreply@roqua.nl>},
-   :exception_recipients => Settings.exception_email
+   # TODO After the 201105 release we can remove this conditional
+   :exception_recipients => (ActiveRecord::Base.connection.column_exists?("settings", "target_id") ? 
+                             Settings.exception_email :
+                             "marten@roqua.nl")
 end
