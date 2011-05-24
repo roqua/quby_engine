0463b297d53382517f8e8db35632bdcc58992197
Marten Veldthuis <marten@veldthuis.com>
2011-05-24 15:46:14 +0200

    we should probably just make exception_notifier initializer run after settings initializer somehow

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -3,5 +3,5 @@ unless Rails.env.development? or (not ActiveRecord::Base.connection.column_exist
    :email_prefix => "[Quby] ",
    :sender_address => %{"Exception Notifier" <noreply@roqua.nl>},
    # TODO After the 201105 release we can remove this conditional
-   :exception_recipients => Settings.exception_email
+   :exception_recipients => Settings.exception_email || "#{Settings.organization}@roqua.nl"
 end
