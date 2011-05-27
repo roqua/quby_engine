426bece37f0606cd9f8678f9fad9fe4005331997
Samuel Esposito <s.esposito@med.umcg.nl>
2011-05-27 14:32:10 +0200

    update end_panel

--- db/functions/end_panel.rb
+++ db/functions/end_panel.rb
@@ -1,5 +1,7 @@
-def end_panel 
+def end_panel
   panel do
-    text "Bedankt voor het invullen van deze vragenlijst. U kunt uw antwoorden opslaan via de &#39;Klaar&#39;-knop onderaan. Daarna kunt u uw antwoorden niet meer wijzigen."
+    text "*Bedankt voor het invullen van deze vragenlijst.*  
+* U kunt uw antwoorden afdrukken door op de knop &#39;Print Antwoorden&#39; de klikken.  
+* Sla de antwoorden op door op de &#39;Klaar&#39;-knop onderaan te klikken. Daarna kunt u uw antwoorden niet meer wijzigen."
   end
 end
