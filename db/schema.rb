2b4fd03952a9ee49a653aa5259a14abdaeb7336c
Samuel Esposito <s.esposito@med.umcg.nl>
2011-06-20 11:29:31 +0200

    remove panss_verkort

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110502124801) do
+ActiveRecord::Schema.define(:version => 20110620092624) do
 
   create_table "answers", :force => true do |t|
     t.text     "value"
