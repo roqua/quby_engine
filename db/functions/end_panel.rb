307a305546e63e66244c6748f61d33bf310b75e0
Samuel Esposito <s.esposito@roqua.nl>
2011-08-11 16:13:05 +0200

    tweak

--- db/functions/end_panel.rb
+++ db/functions/end_panel.rb
@@ -1,7 +1,7 @@
 def end_panel
   panel do
     text "*Bedankt voor het invullen van deze vragenlijst.*  
-* U kunt uw antwoorden afdrukken door op de knop &#39;Print Antwoorden&#39; de klikken.  
+* U kunt uw antwoorden afdrukken door op de knop &#39;Print Antwoorden&#39; te klikken.  
 * Sla de antwoorden op door op de &#39;Klaar&#39;-knop onderaan te klikken. Daarna kunt u uw antwoorden niet meer wijzigen."
   end
 end
