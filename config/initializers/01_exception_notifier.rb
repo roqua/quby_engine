7cd4d4e80c4726a2a5a5c50547c54f9cbb22bb56
Marten Veldthuis <marten@veldthuis.com>
2011-08-12 16:20:41 +0200

    also dont run exceptionnotifier in test env

--- config/initializers/01_exception_notifier.rb
+++ config/initializers/01_exception_notifier.rb
@@ -1,7 +1,6 @@
-unless Rails.env.development? or (not ActiveRecord::Base.connection.column_exists?("settings", "target_id"))
+if not (Rails.env.development? or Rails.env.test?)
   Quby::Application.config.middleware.use ExceptionNotifier,
    :email_prefix => "[Quby] ",
    :sender_address => %{"Exception Notifier" <noreply@roqua.nl>},
-   # TODO After the 201105 release we can remove this conditional
    :exception_recipients => Settings.exception_email || "#{Settings.organization}@roqua.nl"
 end
