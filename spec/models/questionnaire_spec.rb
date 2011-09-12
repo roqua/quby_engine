b16da8f5d7def15744228bbdf8de57de1875388d
Marten Veldthuis <marten@veldthuis.com>
2011-09-12 15:44:55 +0200

    fix specs

--- spec/models/questionnaire_spec.rb
+++ spec/models/questionnaire_spec.rb
@@ -1,29 +1,32 @@
 require 'spec_helper'
 
 describe Questionnaire do
-  before(:each) do
-    @q = Questionnaire.new
+  def quest(definition)
+    questionnaire = Questionnaire.new
+    questionnaire.stub(:definition).and_return(definition)
+    questionnaire.enhance_by_dsl
+    questionnaire
   end
-
+  
   it "should be possible to create a new questionnaire with a key"
   it "should require a key to create"
   
   it "should support panels" do
-    QuestionnaireDsl.enhance(@q, "panel {}")
+    @q = quest("panel {}")
 
     @q.panels.size.should == 1
     @q.panels[0].class.should == Items::Panel
   end
   
   it "should support questions" do
-    QuestionnaireDsl.enhance(@q, "question(:myid, :title => 'Question 1') {}")
+    @q = quest("question(:myid, :title => 'Question 1') {}")
 
     @q.panels.size.should == 1
     @q.panels[0].items[0].class.should == Items::Question
   end
   
   it "should support text" do
-    QuestionnaireDsl.enhance(@q, "text 'The quick brown fox jumps over the lazy dog.'")
+    @q = quest("text 'The quick brown fox jumps over the lazy dog.'")
 
     @q.panels.size.should == 1
     @q.panels[0].items[0].class.should == Items::Text
@@ -43,7 +46,7 @@ describe Questionnaire do
       end
     END
 
-    QuestionnaireDsl.enhance(@q, questionnaire)
+    @q = quest(questionnaire)
     @q.questions.length.should == 3
   end
 
@@ -61,7 +64,7 @@ describe Questionnaire do
       end
     END
 
-    QuestionnaireDsl.enhance(@q, questionnaire)
+    @q = quest(questionnaire)
     @q.questions_tree.should be_an Array
     
     pending "write more expectations"
