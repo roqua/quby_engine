5457b5e397f38547e94c6bf514e47e29fa83f97d
Marten Veldthuis <marten@veldthuis.com>
2011-09-12 15:45:36 +0200

    remove unused before clause

--- spec/models/items/panel_spec.rb
+++ spec/models/items/panel_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Items::Panel do
-  before(:each) do
-    @q = Questionnaire.new
-  end
-
   it "should be possible to make without options" do
     lambda { Items::Panel.new }.should_not raise_error
   end
