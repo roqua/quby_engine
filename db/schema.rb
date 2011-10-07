771beb8f80a0e78ee6ff14b86afd79efc72a7139
Marten Veldthuis <marten@veldthuis.com>
2011-10-07 15:36:06 +0200

    update autogenerated gemlock and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110907113311) do
+ActiveRecord::Schema.define(:version => 20111005120555) do
 
   create_table "answers", :force => true do |t|
     t.text     "value"
