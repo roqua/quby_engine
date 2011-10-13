b118bf1fdfec465beb768c947162a5b51d0f142a
Marten Veldthuis <marten@veldthuis.com>
2011-10-13 10:38:22 +0200

    Also precompile print.css

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Quby
     config.assets.version = '1.0'
   
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w( screen.css dialog.css ie.css admin/screen.css reset.css syntax.css formtastic.css formtastic_changes.css redmond/jquery-ui-1.8.7.custom.css
+    config.assets.precompile += %w( screen.css dialog.css ie.css admin/screen.css reset.css syntax.css formtastic.css formtastic_changes.css redmond/jquery-ui-1.8.7.custom.css print.css
                                     questionnaires.js answers.js disable_keys.js edit_area/edit_area_full.js jquery.ba-hashchange.min.js jquery.placeholder.js printer.js )
   end
 end
