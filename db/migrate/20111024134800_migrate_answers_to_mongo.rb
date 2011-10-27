f9fae211f59df7c212fbd05a66b8c21eb3feae57
Samuel Esposito <samuel.esposito@gmail.com>
2011-10-27 09:53:47 +0200

    remove bloated output

--- db/migrate/20111024134800_migrate_answers_to_mongo.rb
+++ db/migrate/20111024134800_migrate_answers_to_mongo.rb
@@ -5,17 +5,21 @@ class MigrateAnswersToMongo < ActiveRecord::Migration
   end
 
   def up
+    quest_id = nil
     OldAnswer.find_each do |answer|
-      puts "Processing #{answer.id} for #{answer.questionnaire_id}"
+      if answer.questionnaire_id != quest_id
+        quest_id = answer.questionnaire_id
+        puts "Processing answers for #{answer.questionnaire_id}"
+      end
       Answer.create!(:_id => answer.id,
-                      :questionnaire_id => answer.questionnaire_id,
-                      :patient_id => answer.patient_id,
-                      :active => answer.active,
-                      :test => answer.test,
-                      :created_at => answer.created_at,
-                      :updated_at => answer.updated_at,
-                      :value => answer.value,
-                      :token => answer.token)
+                     :questionnaire_id => answer.questionnaire_id,
+                     :patient_id => answer.patient_id,
+                     :active => answer.active,
+                     :test => answer.test,
+                     :created_at => answer.created_at,
+                     :updated_at => answer.updated_at,
+                     :value => answer.value,
+                     :token => answer.token)
     end
   end
 
