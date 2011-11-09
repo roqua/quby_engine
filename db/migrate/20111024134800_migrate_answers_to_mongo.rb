f9f439554252d8848622e848b0670a6538f106d0
Marten Veldthuis <marten@veldthuis.com>
2011-11-09 16:29:19 +0100

    speed up mongoid conversion of answers
    This means that value_by_values doesn't get saved during
    the migration yet. But after deploy we can easily run that
    slowly while the app is online, which is probably better
    than being offline for hours while the database migrates.

--- db/migrate/20111024134800_migrate_answers_to_mongo.rb
+++ db/migrate/20111024134800_migrate_answers_to_mongo.rb
@@ -4,6 +4,21 @@ class MigrateAnswersToMongo < ActiveRecord::Migration
     serialize :value
   end
 
+  class Answer
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    store_in :answers
+
+    identity type: String
+    field :questionnaire_id,  :type => Integer
+    field :value,             :type => Hash
+    field :patient_id,        :type => String
+    field :token,             :type => String
+    field :active,            :type => Boolean, :default => true
+    field :test,              :type => Boolean, :default => false
+  end
+
   def up
     quest_id = nil
     OldAnswer.find_each do |answer|
@@ -11,16 +26,17 @@ class MigrateAnswersToMongo < ActiveRecord::Migration
         quest_id = answer.questionnaire_id
         puts "Processing answers for #{answer.questionnaire_id}"
       end
-      Answer.create!(:_id => answer.id,
-                     :questionnaire_id => answer.questionnaire_id,
-                     :patient_id => answer.patient_id,
-                     :active => answer.active,
-                     :test => answer.test,
-                     :created_at => answer.created_at,
-                     :updated_at => answer.updated_at,
-                     :value => answer.value,
-                     :token => answer.token)
+      Answer.safely.create!(:_id => answer.id,
+                            :questionnaire_id => answer.questionnaire_id,
+                            :patient_id => answer.patient_id,
+                            :active => answer.active,
+                            :test => answer.test,
+                            :created_at => answer.created_at,
+                            :updated_at => answer.updated_at,
+                            :value => answer.value,
+                            :token => answer.token)
     end
+    raise "Sanity check failed" unless OldAnswer.count == Answer.count
   end
 
   def down
