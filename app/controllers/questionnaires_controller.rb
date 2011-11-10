2822712a798dacd58b419f8ce77e0eef995ef865
Marten Veldthuis <marten@veldthuis.com>
2011-11-10 12:43:15 +0100

    fix case where params[:extra_vars] is an empty string

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -11,7 +11,11 @@ class QuestionnairesController < ApplicationController
   def show
     return head(Questionnaire.exists?(:key => params[:id]) ? 200 : 404) if request.head?
     @questionnaire = Questionnaire.find_by_key(params[:id])
-    @extra_vars = params[:extra_vars].to_a.sort_by{|i| i[0].to_i}.map{|i| i[1]} rescue nil
+    unless params[:extra_vars].blank?
+      @extra_vars = params[:extra_vars].to_a.sort_by{|i| i[0].to_i}.map{|i| i[1]} rescue nil
+    else
+      @extra_vars = []
+    end
     @roqua_key = params[:roqua_key]
     if @questionnaire
       respond_with @questionnaire
