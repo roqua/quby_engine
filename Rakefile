38a42174ae5cfff22c9146f252c9eee3f88aac4f
Jorn van de Beek <jornvandebeek@roqua.nl>
2011-12-09 16:07:26 +0100

    Force trace for rake tasks.

--- Rakefile
+++ Rakefile
@@ -4,4 +4,6 @@
 require File.expand_path('../config/application', __FILE__)
 require 'rake'
 
+Rake.application.options.trace = true 
+
 Quby::Application.load_tasks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111028211433) do
+ActiveRecord::Schema.define(:version => 20111130132643) do
 
   create_table "answers", :force => true do |t|
     t.text     "value"
