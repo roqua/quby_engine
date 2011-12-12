21ad8a85ff5121ce8a6a6cb535d8bfb8195c605b
Marten Veldthuis <marten@veldthuis.com>
2011-12-12 13:54:08 +0100

    namespace javascripts

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-//
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require jquery_placeholder
-//= require_self
