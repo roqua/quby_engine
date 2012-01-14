b4f2224420a5c35306073aac55258a057214f8a5
Ando Emerencia <ando.emerencia@gmail.com>
2012-01-14 01:49:07 +0100

    old api: return 404 if questionnaire not found instead of an internal server error

--- app/controllers/quby/questionnaires_controller.rb
+++ app/controllers/quby/questionnaires_controller.rb
@@ -11,16 +11,20 @@ module Quby
 
     def show
       return head(Questionnaire.exists?(params[:id]) ? 200 : 404) if request.head?
-      @questionnaire = Questionnaire.find_by_key(params[:id])
-      unless params[:extra_vars].blank?
-        @extra_vars = params[:extra_vars].to_a.sort_by{|i| i[0].to_i}.map{|i| i[1]} rescue nil
-      else
-        @extra_vars = []
-      end
-      @roqua_key = params[:roqua_key]
-      if @questionnaire
-        respond_with @questionnaire
-      else
+      begin
+        @questionnaire = Questionnaire.find_by_key(params[:id])
+        unless params[:extra_vars].blank?
+          @extra_vars = params[:extra_vars].to_a.sort_by{|i| i[0].to_i}.map{|i| i[1]} rescue nil
+        else
+          @extra_vars = []
+        end
+        @roqua_key = params[:roqua_key]
+        if @questionnaire
+          respond_with @questionnaire
+        else
+          head 404
+        end
+      rescue Quby::Questionnaire::RecordNotFound
         head 404
       end
     end
