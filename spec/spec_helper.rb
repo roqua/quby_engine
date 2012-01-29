ed78e66aa99f2a2df76bb148a5289afa67bcbf0b
Marten Veldthuis <marten@veldthuis.com>
2012-01-29 13:16:05 +0100

    move test/dummy to spec/dummy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ Spork.prefork do
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../test/dummy/config/environment", __FILE__)
+  require File.expand_path("../dummy/config/environment", __FILE__)
   require 'rspec/rails'
   require 'capybara/rspec'
 
