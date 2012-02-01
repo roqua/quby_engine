3685e37ff45e81495973a4a17c547d453c4f3a69
Marten Veldthuis <marten@veldthuis.com>
2012-02-01 13:08:15 +0100

    Add a score calculator

---
+++ app/models/quby/score_calculator.rb
@@ -0,0 +1,33 @@
+module Quby
+  class ScoreCalculator
+    attr_accessor :values
+
+    # Evaluates block within the context of a new calculator
+    # instance. All instance methods are accessible.
+    def self.calculate(options = {}, &block)
+      instance = self.new(options)
+      instance.instance_eval &block
+    end
+
+    def initialize(options = {})
+      @values = options[:values]
+      @score = {}
+    end
+
+    def values(*keys)
+      if keys.empty?
+        @values
+      else
+        keys.each do |key|
+          raise "Key #{key.inspect} not found in values" unless @values.has_key?(key)
+        end
+
+        @values.values_at(*keys)
+      end
+    end
+
+    def sum(values)
+      values.reduce(0, &:+)
+    end
+  end
+end
---
+++ spec/models/quby/score_calculator_spec.rb
@@ -0,0 +1,50 @@
+require_relative "../../../app/models/quby/score_calculator"
+
+module Quby
+  describe ScoreCalculator do
+    describe '.calculate' do
+      it 'calculates the value of a block' do
+        score = stub
+        calculator = ScoreCalculator.calculate { score }.should == score
+      end
+    end
+
+    describe '#initialize' do
+      it 'stores values passed' do
+        calculator = ScoreCalculator.new(values: {v_1: 1})
+        calculator.values.should == {v_1: 1}
+      end
+    end
+
+    describe '#values' do
+      let(:values) { {v_1: 1, v_2: 4, v_3: nil} }
+      let(:calculator) { ScoreCalculator.new(values: values) }
+
+      it 'returns the values hash if no args given' do
+        calculator.values.should == values
+      end
+
+      it 'returns an array of values if args given' do
+        calculator.values(:v_1, :v_2).should == [values[:v_1], values[:v_2]]
+      end
+
+      it 'raises if a value is requested which does not exist' do
+        expect do
+          calculator.values(:unknown)
+        end.to raise_error(/:unknown/)
+      end
+    end
+
+    describe '#sum' do
+      let(:calculator) { ScoreCalculator.new }
+
+      it 'sums values given' do
+        calculator.sum([1, 2, 3, 4]).should == 10
+      end
+
+      it 'sums no values' do
+        calculator.sum([]).should == 0
+      end
+    end
+  end
+end
