6fb24f24376f767c8cd7739e963d21f208d91c76
Marten Veldthuis <marten@veldthuis.com>
2012-02-18 15:29:00 +0100

    fix questionnaire finder

--- app/models/quby/questionnaire_finder.rb
+++ app/models/quby/questionnaire_finder.rb
@@ -1,42 +1,44 @@
-class QuestionnaireFinder
-  class RecordNotFound < StandardError; end
+module Quby
+  class QuestionnaireFinder
+    class RecordNotFound < StandardError; end
 
-  attr_reader :path
-  attr_reader :questionnaire_class
+    attr_reader :path
+    attr_reader :questionnaire_class
 
-  def initialize(path, questionnaire_class = Questionnaire)
-    @path = path
-    @questionnaire_class = questionnaire_class
-    @questionnaire_cache = {}
-  end
-  
-  def all
-    Dir[File.join(@path, "*.rb")].map do |filename|
-      key = File.basename(filename, '.rb')
-      self.new(key)
+    def initialize(path, questionnaire_class = Questionnaire)
+      @path = path
+      @questionnaire_class = questionnaire_class
+      @questionnaire_cache = {}
     end
-  end
-
-  def find(key)
-    if @questionnaire_cache[key]
-      return @questionnaire_cache[key]
-    else
-      definition = File.read(File.join(path, "#{key}.rb"))
-      questionnaire = questionnaire_class.new(key, definition)
-      @questionnaire_cache[key] = questionnaire
+    
+    def all
+      Dir[File.join(@path, "*.rb")].map do |filename|
+        key = File.basename(filename, '.rb')
+        self.new(key)
+      end
     end
-  end
 
-  def find_by_key(key)
-    if exists?(key)
-      self.new(key)
-    else
-      raise RecordNotFound, key
+    def find(key)
+      if @questionnaire_cache[key]
+        return @questionnaire_cache[key]
+      else
+        definition = File.read(File.join(path, "#{key}.rb"))
+        questionnaire = questionnaire_class.new(key, definition)
+        @questionnaire_cache[key] = questionnaire
+      end
     end
-  end
 
-  def exists?(key)
-    path = File.join(path, "#{key}.rb")
-    File.exist?(path)
+    def find_by_key(key)
+      if exists?(key)
+        self.new(key)
+      else
+        raise RecordNotFound, key
+      end
+    end
+
+    def exists?(key)
+      path = File.join(path, "#{key}.rb")
+      File.exist?(path)
+    end
   end
 end
