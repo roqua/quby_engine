f224da6fdd55074a395c3151868614fe3212e8b1
Samuel Esposito <s.esposito@roqua.nl>
2012-03-26 15:15:17 +0200

    add test for quby answer

--- spec/models/quby/answer_spec.rb
+++ spec/models/quby/answer_spec.rb
@@ -4,22 +4,30 @@ module Quby
   describe Answer do
     let(:questionnaire) do
       stub(:key => "foo",
-          :name => "Foo", 
-          :questions => [stub(:key => "q1", 
-                              :type => :open, 
+          :name => "Foo",
+          :questions => [stub(:key => "q1",
+                              :type => :open,
                               :text_var => "a",
                               :question_group => nil,
                               :hidden? => false,
                               :parent => nil,
                               :depends_on => [],
                               :validations => [{:type => :requires_answer}],
-                              :extra_data => {})], 
+                              :extra_data => {})],
           :scores => [],
           :default_answer_value => {})
     end
 
     before do
       Answer.any_instance.stub(:questionnaire) { questionnaire }
+    end
+
+    describe "#scores" do
+      let(:answer) { Answer.create! }
+
+      it "should be initialized as an empty hash" do
+        answer.scores.should eq({})
+      end
     end
 
     describe '#patient_id' do
