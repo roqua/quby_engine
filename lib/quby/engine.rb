45efe8a25e9e0845080a3615ddaf8687e5509a40
Marten Veldthuis <marten@veldthuis.com>
2012-07-18 14:07:09 +0200

    let quby add precompile paths

--- lib/quby/engine.rb
+++ lib/quby/engine.rb
@@ -14,5 +14,10 @@ require 'andand'
 module Quby
   class Engine < Rails::Engine
     isolate_namespace Quby
+
+    initializer "QubyEngine precompile hook" do |app|
+      app.config.assets.precompile += %w(quby/application.css quby/dialog.css quby/redmond/jquery-ui-1.8.7.custom.css quby/print.css)
+      app.config.assets.precompile += %w(quby/application.js quby/questionnaires.js quby/answers.js quby/disable_keys.js quby/jquery.ba-hashchange.min.js quby/jquery.placeholder.js quby/printer.js )
+    end
   end
 end
