b9d8d951f8419c103d2933fcac8b4bdcb3df8248
Marten Veldthuis <marten@veldthuis.com>
2012-10-06 17:42:57 +0200

    refactor specs

--- spec/models/quby/outcome_calculations_spec.rb
+++ spec/models/quby/outcome_calculations_spec.rb
@@ -21,34 +21,35 @@ module Quby
 
     describe '#action' do
       it 'returns :alarm if any score is alarming' do
-        answer = Answer.new(scores: {tot: {label: "Totaal", value: 10, status: "alarm"},
-                                     soc: {label: "Sociaal", value: 5, status: "attention"}})
+        answer.scores = {tot: {label: "Totaal", value: 10, status: "alarm"},
+                         soc: {label: "Sociaal", value: 5, status: "attention"}}
         answer.action.should == :alarm
       end
 
       it 'returns :alarm if an answer to a question is alarming' do
-        answer = Answer.new(actions: {alarm: [:v_1]})
+        answer.actions = {alarm: [:v_1]}
         answer.action.should == :alarm
       end
 
       it 'returns :attention if nothing is alarming and score is attention-worthy' do
-        answer = Answer.new(scores: {tot: {label: 'Totaal', value: 10, status: "attention"}})
+        answer.scores = {tot: {label: 'Totaal', value: 10, status: "attention"}}
         answer.action.should == :attention
       end
 
       it 'returns :attention if nothing is alarming and an answer to a question is attention-worthy' do
-        answer = Answer.new(actions: {alarm: [], attention: [:v_1]})
+        answer.actions = {alarm: [], attention: [:v_1]}
         answer.action.should == :attention
       end
 
       it 'returns nil if all scores and answers are neither alarming nor attention-worthy' do
-        answer = Answer.new(scores: {tot: {label: 'Totaal', value: 10}}, actions: {alarm: [], attention: []})
+        answer.scores = {tot: {label: 'Totaal', value: 10}}
+        answer.actions = {alarm: [], attention: []}
         answer.action.should be_nil
       end
 
       it 'works with symbols as well as keys for score statusses' do
-        answer = Answer.new(scores: {tot: {label: "Totaal", value: 10, status: :alarm},
-                                     soc: {label: "Sociaal", value: 5, status: "attention"}})
+        answer.scores = {tot: {label: "Totaal", value: 10, status: :alarm},
+                         soc: {label: "Sociaal", value: 5, status: "attention"}}
         answer.action.should == :alarm
       end
     end
@@ -56,7 +57,6 @@ module Quby
     describe '#calculate_scores' do
       it 'calculates scores' do
         score.stub(:calculation => Proc.new { 3 })
-        answer = Answer.new(value: {})
         answer.calculate_scores.should == {:tot => 3}
       end
 
@@ -68,7 +68,7 @@ module Quby
                                                  stub(:key => :a1, :value => 2)
                                                  ],
                                                :text_var => false)])
-        answer = Answer.new(value: {'v1' => :a1})
+        answer.value = {'v1' => :a1}
         answer.calculate_scores.should == {:tot => [2]}
       end
 
@@ -77,7 +77,6 @@ module Quby
         score2 = stub(key: :two, calculation: Proc.new { {value: score(:one)[:value] + 2} })
         scores = [score1, score2]
         questionnaire.stub(scores: scores)
-        answer = Answer.new(value: {})
         answer.calculate_scores.should == {one: {value: 4}, two: {value: 6}}
       end
 
@@ -96,32 +95,27 @@ module Quby
 
     describe '#update_scores' do
       it 'calculates scores' do
-        answer = Answer.new(value: {})
         answer.should_receive(:calculate_scores)
         answer.should_receive(:calculate_actions)
         answer.update_scores
       end
 
       it 'assigns the calculated score to self.scores' do
-        answer = Answer.new(value: {})
         answer.update_scores
         answer.scores.should == {'tot' => 3}
       end
 
       it 'assigns the calculated actions to self.actions' do
-        answer = Answer.new(value: {})
         answer.update_scores
         answer.actions.should == {'attention' => 5}
       end
 
       it 'skips the set_score callback' do
-        answer = Answer.new(value: {})
         answer.update_scores
         answer.should_not_receive(:set_scores)
       end
 
       it 'skips the set_actions callback' do
-        answer = Answer.new(value: {})
         answer.update_scores
         answer.should_not_receive(:set_actions)
       end
