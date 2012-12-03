46272cc0eeb7e92088660d62128352180a8a00f8
Marten Veldthuis <marten@veldthuis.com>
2012-12-03 22:28:04 +0100

    Get rid of code junk

--- spec/requests/quby/answers_spec.rb
+++ spec/requests/quby/answers_spec.rb
@@ -57,38 +57,5 @@ module Quby
         flash[:notice].should match(/nog niet volledig ingevuld/)
       end
     end
-
-    # Questionnaire.all.each do |questionnaire|
-      # describe "GET /questionnaires/#{questionnaire.key}/answers/some_id" do
-        #before(:each) do
-          #@answer = Answer.create!(:test => true,
-                                  #:value => questionnaire.default_answer_value,
-                                  #:questionnaire_id => questionnaire.id)
-          #@timestamp = Time.now.getgm.strftime("%Y-%m-%dT%H:%M:%S+00:00")
-          #@plain_hmac = [::Settings.shared_secret, @answer.token, @timestamp].join('|')
-          #@hmac = Digest::SHA1.hexdigest(@plain_hmac)
-        #end
-
-        #it "should return HTTP 200 in paged mode" do
-          #path = edit_questionnaire_answer_path(questionnaire, @answer,
-                                              #:token => @answer.token,
-                                              #:display_mode => 'paged',
-                                              #:timestamp => @timestamp,
-                                              #:hmac => @hmac)
-          #get path
-          #response.status.should be(200)
-        #end
-
-        #it "should return HTTP 200 in bulk mode" do
-          #path = edit_questionnaire_answer_path(questionnaire, @answer,
-                                              #:token => @answer.token,
-                                              #:display_mode => 'bulk',
-                                              #:timestamp => @timestamp,
-                                              #:hmac => @hmac)
-          #get path
-          #response.status.should be(200)
-        #end
-      # end
-    # end
   end
 end
