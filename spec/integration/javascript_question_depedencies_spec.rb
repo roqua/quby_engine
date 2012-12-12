46441df82a062d52a07cc835ada3f3264becc6f4
Marten Veldthuis <marten@veldthuis.com>
2012-12-12 15:04:24 +0100

    add bunch of pending specs for other dependencies

--- spec/integration/javascript_question_depedencies_spec.rb
+++ spec/integration/javascript_question_depedencies_spec.rb
@@ -6,26 +6,42 @@ feature 'Dependencies between questions', js: true do
   # here. Hence the weird CSS-selector dependency that is more prone
   # to breakage if we ever change the HTML.
 
-  scenario 'options can hide other questions' do
-    questionnaire = inject_questionnaire("test", <<-END)
-      panel do
-      question :v1, type: :radio, required: true do
-        title "Verbergt vraag 2"
-        option :a1, value: 1, description: "Ja", :hides_questions => [:v2]
-        option :a2, value: 2, description: "Nee"
-      end; 
-      
-      question :v2, type: :string, required: true do
-        title "Vul dit in"
-      end
-      end; end_panel
-    END
+  context 'radio questions' do
+    scenario 'options can hide other questions' do
+      questionnaire = inject_questionnaire("test", <<-END)
+        panel do
+        question :v1, type: :radio, required: true do
+          title "Verbergt vraag 2"
+          option :a1, value: 1, description: "Ja", :hides_questions => [:v2]
+          option :a2, value: 2, description: "Nee"
+        end; 
+        
+        question :v2, type: :string, required: true do
+          title "Vul dit in"
+        end
+        end; end_panel
+      END
 
-    visit_new_answer_for(questionnaire)
+      visit_new_answer_for(questionnaire)
 
-    within("#item_v1") { choose "Nee" }
-    find("#item_v2").find('.main').should be_visible
-    within("#item_v1") { choose "Ja" }
-    find("#item_v2").find('.main').should_not be_visible
+      within("#item_v1") { choose "Nee" }
+      find("#item_v2").find('.main').should be_visible
+      within("#item_v1") { choose "Ja" }
+      find("#item_v2").find('.main').should_not be_visible
+      within("#item_v1") { choose "Nee" }
+      find("#item_v2").find('.main').should be_visible
+    end
+    
+    scenario 'subquestions are disabled unless parent option selected'
+  end
+
+  context 'checkbox questions' do
+    scenario 'uncheck_all'
+    scenario 'check_all'
+  end
+
+  context 'question groups' do
+    scenario 'minumum number of answered of group'
+    scenario 'maximum number of answered of group'
   end
 end
