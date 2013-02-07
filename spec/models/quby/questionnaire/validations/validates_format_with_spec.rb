b2b36fa872284ba7cc7021b39cd73c4ce8567d26
Jorn van de Beek <jornvandebeek@gmail.com>
2013-02-07 15:43:33 +0100

    Fixed title questions being deleted on validation [Delivers #44010047]

--- spec/models/quby/questionnaire/validations/validates_format_with_spec.rb
+++ spec/models/quby/questionnaire/validations/validates_format_with_spec.rb
@@ -7,14 +7,30 @@ module Quby
         before(:each) do
           @questionnaire = Quby::Questionnaire.new("test")
           @questionnaire.stub(:definition).and_return(<<-END)
-            question :one, :type => :string do
-              title "Testvraag"
+            question :v_1, :type => :string do
+              title "Testquestion"
               validates_format_with /right/
             end
 
-            question :two, :type => :string do
-              title "Vraag 2"
+            question :v_2, :type => :string do
+              title "Question 2"
               validates_format_with /qwerty/, :message => "should contain qwerty"
+            end
+
+            question :v_3, :type => :radio do
+              title "Question 3 with title question"
+              title_question :v_3_01, :type => :string, :title => "Geef aan:", :depends_on => [:v_3_a2]
+
+              option :a1, :value => 0
+              option :a2, :value => 1
+            end
+
+            question :v_4, :type => :radio do
+              title "Question 4 with subquestion"
+              option :a1, :value => 0 do
+                question :v_4_01, :type => :string
+              end
+              option :a2, :value => 1
             end
           END
           @questionnaire.enhance_by_dsl
@@ -29,20 +45,38 @@ module Quby
         end
 
         it "should check if the value matches the regexp" do
-          @answer.value = {"one" => "right"}
+          @answer.value = {"v_1" => "right"}
           @answer.validate_answers
           @answer.errors[:one].any?.should be_false
 
-          @answer.value = {"one" => "wrong"}
+          @answer.value = {"v_1" => "wrong"}
           @answer.validate_answers
-          @answer.errors[:one].any?.should be_true
+          @answer.errors[:v_1].any?.should be_true
         end
 
         it "should add a message if passed" do
-          @answer.value = {"one" => "right", "two" => "wrong"}
+          @answer.value = {"v_1" => "right", "v_2" => "wrong"}
           @answer.validate_answers
-          @answer.errors[:two].any?.should be_true
-          @answer.errors[:two].map{|i| i[:message] }.should include("should contain qwerty")
+          @answer.errors[:v_2].any?.should be_true
+          @answer.errors[:v_2].map{|i| i[:message] }.should include("should contain qwerty")
+        end
+
+        it "preserves title questions" do
+          @answer.value = {"v_3_01" => "answer"}
+          @answer.validate_answers
+          @answer.value["v_3_01"].should == "answer"
+        end
+
+        it "preserves subquestions if the parent option is ticked" do
+          @answer.value = {"v_4" => "a1", "v_4_01" => "answer"}
+          @answer.validate_answers
+          @answer.value["v_4_01"].should == "answer"
+        end
+
+        it "deletes subquestions of options that are not ticked" do
+          @answer.value = {"v_4_01" => "answer"}
+          @answer.validate_answers
+          @answer.value["v_4_01"].should be_nil
         end
       end
     end
