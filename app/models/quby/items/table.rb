fb8419a16bccf2c8c6402ab211da6fe5243e013a
Marten Veldthuis <marten@veldthuis.com>
2013-02-18 10:33:48 +0100

    Fix is_a

--- app/models/quby/items/table.rb
+++ app/models/quby/items/table.rb
@@ -1,29 +1,29 @@
 module Quby
   class Items::Table < Item
-    
+
     attr_accessor :columns
     attr_accessor :items
-    
+
     attr_accessor :title
     attr_accessor :description
-    
+
     # Whether the question options in this table should show their descriptions
     attr_accessor :show_option_desc
-    
+
     def initialize(options = {})
       @columns = options[:columns]
       @title = options[:title]
-      @description = options[:description]    
+      @description = options[:description]
       @show_option_desc = options[:show_option_desc] || false
       @items = []
     end
-    
+
     def item_table
       rows
-      @item_table    
+      @item_table
     end
-    
-    #FIXME: code to be ashamed of 
+
+    #FIXME: code to be ashamed of
     def rows
       return @rows if @rows
       @item_table = [[]]
@@ -31,10 +31,10 @@ module Quby
       filled_columns = 0
       filled_rows = 0
       row_items = 0
-      
+
       skips = []
       items.each do |item|
-        
+
         skips.delete_if do |row_span, skip_cols_at, skip_cols_length|
           next true if row_span == 1
           if skip_cols_length > 0 and filled_columns == skip_cols_at
@@ -60,17 +60,17 @@ module Quby
             next row_span -1 == 1
           end
         end
-        
-        if item.is_a(Quby::Items::Text) or not ([:check_box, :radio, :scale].include? item.type)
+
+        if item.is_a?(Quby::Items::Text) or not ([:check_box, :radio, :scale].include? item.type)
           if item.row_span > 1
             skips << [item.row_span, filled_columns, item.col_span]
           end
-        
+
           @item_table[filled_rows] << item
           @rows[filled_rows][row_items] << item
           filled_columns += item.col_span
           #puts "#{item.class.name} #{item.type} #{item.col_span}"
-          
+
           if filled_columns >= columns and item != items.last
             filled_rows += 1
             filled_columns = 0
@@ -83,19 +83,19 @@ module Quby
             row_items += 1
             @rows[filled_rows] << []
           end
-          
+
         else #is :check_box, :radio or :scale question
           if item.row_span > 1
             skips << [item.row_span, filled_columns, item.options.length]
           end
-        
+
           @item_table[filled_rows] << item
           if item.options.length <= columns #multiple questions on one row
             item.options.each do |opt|
-              @rows[filled_rows][row_items] << opt            
+              @rows[filled_rows][row_items] << opt
               filled_columns += 1
-              
-              if filled_columns >= columns and item != items.last 
+
+              if filled_columns >= columns and item != items.last
                 filled_rows += 1
                 filled_columns = 0
                 row_items = 0
@@ -128,10 +128,10 @@ module Quby
       end
       @rows
     end
-    
+
     def type
       "table"
     end
-    
+
   end
 end
