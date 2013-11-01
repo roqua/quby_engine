a3ea55042219ab0f091e1c2aef5699c1ecc62621
Marten Veldthuis <marten@veldthuis.com>
2013-11-01 11:05:47 +0100

    Fix rubocop AlignArray

--- app/helpers/quby/layout_helper.rb
+++ app/helpers/quby/layout_helper.rb
@@ -1,7 +1,8 @@
 module Quby
   module LayoutHelper
     def class_for_current_controller_and_action
-      [request.path_parameters[:controller].split("/").join(" "), 
+      [
+        request.path_parameters[:controller].split("/").join(" "),
         request.path_parameters[:action]
       ].join(" ")
     end
