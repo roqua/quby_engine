3e538f640103cf29873bd3691165a04e6f99f4ad
Marten Veldthuis <marten@veldthuis.com>
2013-11-01 12:35:20 +0100

    Remove unused method

--- app/helpers/quby/answers_helper.rb
+++ app/helpers/quby/answers_helper.rb
@@ -1,14 +1,5 @@
 module Quby
   module AnswersHelper
-    def render_question(id, question)
-  #    case question.type
-  #    when :open
-  #      render :partial => "open_question",  :locals => { :id => id, :question => question }
-  #    when :radio
-        render :partial => "radio_question", :locals => { :id => id, :question => question }
-  #    end
-    end
-
     def get_validation_json(validations)
       result = {}
       validations.each_pair do |qkey, valar|
