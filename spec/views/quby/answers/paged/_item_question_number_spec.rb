6984d0baad90ab4866212cb3cfecb11c8c56c201
Marten Veldthuis <marten@veldthuis.com>
2013-11-01 13:24:25 +0100

    Convert spec/views to 1.9 hash syntax

--- spec/views/quby/answers/paged/_item_question_number_spec.rb
+++ spec/views/quby/answers/paged/_item_question_number_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 module Quby
   describe "quby/answers/paged/_item_question_number" do
     let(:question) do
-      stub(:key => :q1,
-           :description => "",
-           :title => "Titel!",
-           :left_label => nil,
-           :right_label => nil,
-           :html_id => "",
-           :type => :integer,
-           :as => :slider,
-           :validations => [{:type => :minimum, :value => 1},
-                            {:type => :maximum, :value => 20}],
-           :autocomplete => false,
-           :disabled => false,
-           :size => nil,
-           :unit => nil)
+      stub(key: :q1,
+           description: "",
+           title: "Titel!",
+           left_label: nil,
+           right_label: nil,
+           html_id: "",
+           type: :integer,
+           as: :slider,
+           validations: [{type: :minimum, value: 1},
+                         {type: :maximum, value: 20}],
+           autocomplete: false,
+           disabled: false,
+           size: nil,
+           unit: nil)
     end
 
-    before { view.stub(:marukufix => "HOI") }
+    before { view.stub(marukufix: "HOI") }
 
     it 'renders a left and right label if both provided' do
-      question.stub(:left_label => "mijn left label")
-      question.stub(:right_label => "mijn right label")
-      render :partial => "quby/answers/paged/item_question_number", :locals => {question: question, subquestion: false, disabled: false}
+      question.stub(left_label: "mijn left label")
+      question.stub(right_label: "mijn right label")
+      render partial: "quby/answers/paged/item_question_number", locals: {question: question, subquestion: false, disabled: false}
       rendered.should include("mijn left label")
       rendered.should include("mijn right label")
     end
 
     it 'sets a floating step size for float questions' do
       question.stub(type: :float)
-      render :partial => "quby/answers/paged/item_question_number", :locals => {question: question, subquestion: false, disabled: false}
+      render partial: "quby/answers/paged/item_question_number", locals: {question: question, subquestion: false, disabled: false}
       rendered.should include("step=\"0.01\"")
     end
   end
