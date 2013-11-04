8886c52974092a64cac1002cadaa5991f1241355
Marten Veldthuis <marten@veldthuis.com>
2013-11-04 15:51:00 +0100

    Refactor clear_and_skip

--- app/models/quby/answer_validations.rb
+++ app/models/quby/answer_validations.rb
@@ -35,15 +35,25 @@ module Quby
       calculated_attributes.groups
     end
 
+    def parent_option_is_not_selected(question)
+      return false unless question.parent and question.parent_option_key
+
+      case question.parent.type
+      when :radio
+        value[question.parent.key.to_s] != question.parent_option_key.to_s
+      when :check_box
+        value[question.parent.key.to_s].andand[question.parent_option_key.to_s] != 1
+      else
+        false
+      end
+    end
+
     def clear_and_skip?(answer, question)
-      answer == "DESELECTED_RADIO_VALUE" or
-      answer == question.extra_data[:placeholder].to_s or
-        (question.parent and question.parent_option_key and
-              ((question.parent.type == :radio     and
-                  value[question.parent.key.to_s] != question.parent_option_key.to_s) or
-                  (question.parent.type == :check_box and
-                      value[question.parent.key.to_s].andand[question.parent_option_key.to_s] != 1))) or
-      hidden_questions.andand.include?(question.key)
+      return true if answer == "DESELECTED_RADIO_VALUE"
+      return true if answer == question.extra_data[:placeholder].to_s
+      return true if parent_option_is_not_selected(question)
+      return true if hidden_questions.andand.include?(question.key)
+      false
     end
 
     def validate_answers
