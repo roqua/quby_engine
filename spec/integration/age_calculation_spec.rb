358e9290de84fe98d3d2c6893d11879125b37867
Jorn van de Beek <jornvandebeek@gmail.com>
2013-11-11 16:24:43 +0100

    Fix age calculation spec [#60499034]

--- spec/integration/age_calculation_spec.rb
+++ spec/integration/age_calculation_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 module Quby
   describe 'Age in scores' do
-    let!(:questionnaire) { Quby::Questionnaire.find_by_key('scared') }
+    let!(:questionnaire) { Quby.questionnaire_finder.find('scared') }
     let(:answer) { Answer.new(questionnaire_key: 'scared', patient: {"birthyear" => 2000, "gender" => :male}) }
     let(:updates_answers) { UpdatesAnswers.new answer }
 
     it 'can access age in a real score calculation' do
       answer.stub(:completed?).and_return true
       updates_answers.update
-      answer.scores.should == {"tot"=>{"label"=>"Totaalscore", "value"=>10, "interpretation"=>"Laag", "score"=>true}}
+      answer.scores.should == {"tot" => {"label" => "Totaalscore",
+                                         "value" => 10,
+                                         "interpretation" => "Laag",
+                                         "score" => true}}
     end
   end
 end
