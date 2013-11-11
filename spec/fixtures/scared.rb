437e1b22a5ca62af4fb1999f39051d8f7f39c8f8
Jorn van de Beek <jornvandebeek@gmail.com>
2013-11-11 16:08:01 +0100

    Set completed_at before calculating scores. [#60499034]

---
+++ spec/fixtures/scared.rb
@@ -0,0 +1,61 @@
+title "SCARED-NL"
+
+panel do
+end
+
+score :tot, :label => "Totaalscore" do
+  tot = 10
+  if gender != :unknown && age
+    if gender == :male
+      if (7..12).include? age
+        if tot >= 66
+          tot_interpretation = "Risico"
+        elsif tot >= 60
+          tot_interpretation = "Verhoogd"
+        elsif tot >= 19
+          tot_interpretation = "Normaal"
+        else
+          tot_interpretation = "Laag"
+        end
+      elsif (13..19).include? age
+        if tot >= 50
+          tot_interpretation = "Risico"
+        elsif tot >= 48
+          tot_interpretation = "Verhoogd"
+        elsif tot >= 13
+          tot_interpretation = "Normaal"
+        else
+          tot_interpretation = "Laag"
+        end
+      end
+    elsif gender == :female
+      if (7..12).include? age
+        if tot >= 76
+          tot_interpretation = "Risico"
+        elsif tot >= 70
+          tot_interpretation = "Verhoogd"
+        elsif tot >= 28
+          tot_interpretation = "Normaal"
+        else
+          tot_interpretation = "Laag"
+        end
+      elsif (13..19).include? age
+        if tot >= 62
+          tot_interpretation = "Risico"
+        elsif tot >= 60
+          tot_interpretation = "Verhoogd"
+        elsif tot >= 23
+          tot_interpretation = "Normaal"
+        else
+          tot_interpretation = "Laag"
+        end
+      end
+    end
+  end
+
+  {
+      :label => "Totaalscore",
+      :value => tot,
+      :interpretation => tot_interpretation
+  }
+end
