0fafca0eb272590575736bded520751840394ae7
Marten Veldthuis <marten@veldthuis.com>
2013-11-27 11:34:12 +0100

    Add server-side validation of the range of a date question

--- spec/integration/question_types/float_question_spec.rb
+++ spec/integration/question_types/float_question_spec.rb
@@ -1,36 +1,73 @@
 require 'spec_helper'
 
 feature 'Float questions' do
-  let(:questionnaire) do
-    inject_questionnaire "test", <<-END
-      question :v_float, type: :float, required: true do
-        title "Enter a float"
-      end
-    END
-  end
-
   let(:answer) { Quby::Answer.create(questionnaire_key: questionnaire.key) }
   let(:updates_answers) { Quby::UpdatesAnswers.new(answer) }
 
-  scenario 'saving a valid float' do
-    updates_answers.update('v_float' => '1')
-    answer.errors.should_not be_present
-    answer.reload.v_float.should == '1'
+  context 'requires_answer validation' do
+    let(:questionnaire) do
+      inject_questionnaire "test", <<-END
+        question :v_float, type: :float, required: true do
+          title "Enter a float"
+        end
+      END
+    end
+
+    scenario 'saving a valid float' do
+      updates_answers.update('v_float' => '1.2')
+      answer.errors.should_not be_present
+      answer.reload.v_float.should == '1.2'
+    end
+
+    scenario 'saving an integer into a float field' do
+      updates_answers.update('v_float' => '1')
+      answer.errors.should_not be_present
+      answer.reload.v_float.should == '1'
+    end
+
+    scenario 'saving an invalid float' do
+      updates_answers.update('v_float' => 'foo')
+      answer.errors[:v_float].should be_present
+
+      answer.errors.clear
+      updates_answers.update('v_float' => '')
+      answer.errors[:v_float].should be_present
+    end
   end
 
-  scenario 'saving a float' do
-    updates_answers.update('v_float' => '1.2')
-    answer.errors.should_not be_present
-    answer.reload.v_float.should == '1.2'
-  end
+  context 'in_range validation' do
+    let(:questionnaire) do
+      inject_questionnaire "test", <<-END
+        question :v_float, type: :float do
+          title "Enter a date"
+          validates_in_range 10..20
+        end
+      END
+    end
 
-  scenario 'saving an invalid float' do
-    updates_answers.update('v_float' => 'foo')
-    answer.errors[:v_float].should be_present
-  end
+    scenario 'saving a valid float' do
+      updates_answers.update('v_float' => '15')
+      answer.errors.should_not be_present
+      answer.reload.v_float.should == '15'
+    end
 
-  scenario 'saving without a float' do
-    updates_answers.update('v_float' => '')
-    answer.errors[:v_float].should be_present
+    scenario 'saving a too-low float' do
+      updates_answers.update('v_float' => '5')
+      answer.errors[:v_float].should eq([{message: 'Smaller than minimum', valtype: :minimum}])
+    end
+
+    scenario 'saving a too-high float' do
+      updates_answers.update('v_float' => '25')
+      answer.errors[:v_float].should eq([{message: 'Exceeds maximum', valtype: :maximum}])
+    end
+
+    scenario 'saving an invalid float' do
+      updates_answers.update('v_float' => 'foo')
+      answer.errors[:v_float].should be_present
+
+      answer.errors.clear
+      updates_answers.update('v_float' => '')
+      answer.errors[:v_float].should be_present
+    end
   end
 end
--- spec/integration/question_types/integer_question_spec.rb
+++ spec/integration/question_types/integer_question_spec.rb
@@ -1,35 +1,72 @@
 require 'spec_helper'
 
 feature 'Integer questions' do
-  let(:questionnaire) do
-    inject_questionnaire "test", <<-END
-      question :v_integer, type: :integer, required: true do
-        title "Enter a integer"
-      end
-    END
-  end
-
   let(:answer) { Quby::Answer.create(questionnaire_key: questionnaire.key) }
   let(:updates_answers) { Quby::UpdatesAnswers.new(answer) }
 
-  scenario 'saving a valid integer' do
-    updates_answers.update('v_integer' => '1')
-    answer.errors.should_not be_present
-    answer.reload.v_integer.should == '1'
+  context 'requires_answer validation' do
+    let(:questionnaire) do
+      inject_questionnaire "test", <<-END
+        question :v_integer, type: :integer, required: true do
+          title "Enter a integer"
+        end
+      END
+    end
+
+    scenario 'saving a valid integer' do
+      updates_answers.update('v_integer' => '1')
+      answer.errors.should_not be_present
+      answer.reload.v_integer.should == '1'
+    end
+
+    scenario 'saving a float into integer field' do
+      updates_answers.update('v_integer' => '1.2')
+      answer.errors[:v_integer].should be_present
+    end
+
+    scenario 'saving an invalid integer' do
+      updates_answers.update('v_integer' => 'foo')
+      answer.errors[:v_integer].should be_present
+
+      answer.errors.clear
+      updates_answers.update('v_integer' => '')
+      answer.errors[:v_integer].should be_present
+    end
   end
 
-  scenario 'saving a float' do
-    updates_answers.update('v_integer' => '1.2')
-    answer.errors[:v_integer].should be_present
-  end
+  context 'in_range validation' do
+    let(:questionnaire) do
+      inject_questionnaire "test", <<-END
+        question :v_integer, type: :integer do
+          title "Enter a date"
+          validates_in_range 10..20
+        end
+      END
+    end
 
-  scenario 'saving an invalid integer' do
-    updates_answers.update('v_integer' => 'foo')
-    answer.errors[:v_integer].should be_present
-  end
+    scenario 'saving a valid integer' do
+      updates_answers.update('v_integer' => '15')
+      answer.errors.should_not be_present
+      answer.reload.v_integer.should == '15'
+    end
 
-  scenario 'saving without a integer' do
-    updates_answers.update('v_integer' => '')
-    answer.errors[:v_integer].should be_present
+    scenario 'saving a too-low integer' do
+      updates_answers.update('v_integer' => '5')
+      answer.errors[:v_integer].should eq([{message: 'Smaller than minimum', valtype: :minimum}])
+    end
+
+    scenario 'saving a too-high integer' do
+      updates_answers.update('v_integer' => '25')
+      answer.errors[:v_integer].should eq([{message: 'Exceeds maximum', valtype: :maximum}])
+    end
+
+    scenario 'saving an invalid integer' do
+      updates_answers.update('v_integer' => 'foo')
+      answer.errors[:v_integer].should be_present
+
+      answer.errors.clear
+      updates_answers.update('v_integer' => '')
+      answer.errors[:v_integer].should be_present
+    end
   end
 end
