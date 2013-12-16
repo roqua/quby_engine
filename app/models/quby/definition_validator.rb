9449390be66ee58e2391dbcd1aa4d310bffca607
Ivan Malykh <ivan@roqua.nl>
2013-12-16 09:50:58 +0100

    added validation for check_box questions; refactored definition_validator and its spec

--- app/models/quby/definition_validator.rb
+++ app/models/quby/definition_validator.rb
@@ -18,10 +18,7 @@ module Quby
       functions = Function.all.map(&:definition).join("\n\n")
       QuestionnaireDsl.enhance(q, [functions, definition].join("\n\n"))
 
-      check_if_to_be_hidden_questions_actually_exist(q)
-
-      validate_question_key_length(q)
-      validate_question_key_starts_with_v_underscore(q)
+      validate_questions(q)
 
       true
     # Some compilation errors are Exceptions (pure syntax errors) and some StandardErrors (NameErrors)
@@ -31,35 +28,44 @@ module Quby
       false
     end
 
-    def check_if_to_be_hidden_questions_actually_exist(q)
-      q.questions.compact.each do |question|
-        question.options.each do |option|
-          if option.hides_questions.present?
-            option.hides_questions.each do |key|
-              unless q.question_hash[key]
-                raise "Question #{question.key} option #{option.key} hides nonexistent question #{key}"
-              end
-            end
+    def validate_questions(q)
+      compact_questions(q).each do |question|
+        check_if_to_be_hidden_questions_actually_exist question
+
+        validate_key_format question.year_key
+        validate_key_format question.month_key
+        validate_key_format question.day_key
+      end
+
+      q.question_hash.keys.each do |key|
+        validate_key_format key
+      end
+    end
+
+    def check_if_to_be_hidden_questions_actually_exist(question)
+      question.options.each do |option|
+        next if option.hides_questions.blank?
+        option.hides_questions.each do |key|
+          unless q.question_hash[key]
+            raise "Question #{question.key} option #{option.key} hides nonexistent question #{key}"
           end
         end
       end
     end
 
-    def validate_question_key_length(q)
-      q.questions.compact.each do |question|
-        if question.key.to_s.length > MAX_KEY_LENGTH
-          raise "Key van de vraagdefinities mag niet langer dan #{MAX_KEY_LENGTH} karakters zijn.<br />" +
-                "Vervang `#{question.key}` met een kortere key."
-        end
+    private
+
+    def validate_key_format(key)
+      if key.to_s.length > MAX_KEY_LENGTH
+        raise "Key `#{key}` mag niet langer zijn dan #{MAX_KEY_LENGTH} karakters."
+      end
+      if not key.to_s.start_with?(KEY_PREFIX)
+        raise "Key `#{key}` moet beginnen met een `#{KEY_PREFIX}`."
       end
     end
 
-    def validate_question_key_starts_with_v_underscore(q)
-      q.questions.compact.each do |question|
-        unless question.key.to_s.start_with?(KEY_PREFIX)
-          raise "Keys van de vraagdefinities moeten beginnen met `#{KEY_PREFIX}`. Was `#{question.key}`."
-        end
-      end
+    def compact_questions(q)
+      q.questions.compact
     end
 
   end
--- spec/models/quby/definition_validator_spec.rb
+++ spec/models/quby/definition_validator_spec.rb
@@ -3,44 +3,101 @@ require 'spec_helper'
 module Quby
   describe DefinitionValidator do
     let(:questionnaire) { Questionnaire.new('test') }
-    let(:invalid_definition) do
-      <<-END
-        question :one, type: :radio do
-          title "Testvraag"
-          option :a1, hides_questions: [:two]
-        end
-      END
-    end
 
     describe ":hides_questions" do
       it "throws an error if the question to be hidden does not exist" do
+        invalid_definition = <<-END
+          question :v_1, type: :radio do
+            title "Testvraag"
+            option :a1, hides_questions: [:v_2]
+          end
+        END
         DefinitionValidator.new(questionnaire, invalid_definition).validate.should be_false
       end
     end
 
     describe ":validates_question_key_format" do
-
-      long_key = <<-END
-          question :morethanten, type: :radio do
-            title "Testvraag"
-            option :a1
-          end
-        END
-
-      short_key = <<-END
-          question :v_1, type: :radio do
-            title "Testvraag"
-            option :a1
-          end
-        END
-
       it "validates length of the question keys" do
+        long_key = <<-END
+          question :questionthree, type: :radio do
+            title "Testvraag"
+            option :a1
+          end
+        END
+        valid_key = <<-END
+          question :v_2, type: :radio do
+            title "Testvraag"
+            option :a1
+          end
+        END
         DefinitionValidator.new(questionnaire, long_key).validate.should be_false
-        DefinitionValidator.new(questionnaire, short_key).validate.should be_true
+        DefinitionValidator.new(questionnaire, valid_key).validate.should be_true
       end
 
       it "validates that question key starts with a `v_`" do
-        DefinitionValidator.new(questionnaire, short_key).validate.should be_true
+        invalid_key = <<-END
+          question :one, type: :radio do
+            title "Testvraag"
+            option :a1, hides_questions: [:two]
+          end
+        END
+        valid_key = <<-END
+          question :v_2, type: :radio do
+            title "Testvraag"
+            option :a1
+          end
+        END
+        DefinitionValidator.new(questionnaire, invalid_key).validate.should be_false
+        DefinitionValidator.new(questionnaire, valid_key).validate.should be_true
+      end
+
+      it "validates check_box question options start with `v_`" do
+        invalid_keys = <<-END
+          question :v_4, type: :check_box do
+            title "Testvraag met een check_box"
+            option :q1
+          end
+        END
+        valid_keys = <<-END
+          question :v_4, type: :check_box do
+            title "Testvraag met een check_box"
+            option :v_q1
+          end
+        END
+        DefinitionValidator.new(questionnaire, invalid_keys).validate.should be_false
+        DefinitionValidator.new(questionnaire, valid_keys).validate.should be_true
+      end
+
+      it "validates check_box question options length" do
+        invalid_keys = <<-END
+          question :v_4, type: :check_box do
+            title "Testvraag met een check_box"
+            option :v_q1_has_a_very_long_key
+          end
+        END
+        valid_keys = <<-END
+          question :v_4, type: :check_box do
+            title "Testvraag met een check_box"
+            option :v_q1
+          end
+        END
+        DefinitionValidator.new(questionnaire, invalid_keys).validate.should be_false
+        DefinitionValidator.new(questionnaire, valid_keys).validate.should be_true
+      end
+
+      it "validates question date keys" do
+        invalid_keys = <<-END
+          question :v_6, type: :date, year_key: :invalid_key, month_key: :v_62, day_key: :v_63 do
+            title "Testvraag met een datum"
+          end
+        END
+        valid_keys = <<-END
+          question :v_6, type: :date, year_key: :v_61, month_key: :v_62, day_key: :v_63 do
+            title "Testvraag met een datum"
+          end
+        END
+        DefinitionValidator.new(questionnaire, invalid_keys).validate.should be_false
+        DefinitionValidator.new(questionnaire, valid_keys).validate.should be_true
       end
     end
   end
