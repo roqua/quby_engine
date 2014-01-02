6c5f950ad5894067605d33498b7800b00c886442
Ivan Malykh <ivan@roqua.nl>
2014-01-02 16:30:28 +0100

    added spec, fixture and helpers for progress test

---
+++ spec/fixtures/panels.rb
@@ -0,0 +1,25 @@
+title "Panels"
+
+panel do
+  question :v_8, type: :radio do
+    title 'Title 1'
+    option :a1, description: 'Ja'
+    option :a2, description: 'Nee'
+  end
+end
+
+panel do
+  question :v_2, type: :radio do
+    title 'Title 2'
+    option :a1, description: 'Ja'
+    option :a2, description: 'Nee'
+  end
+end
+
+panel do
+  question :v_3, type: :radio do
+    title 'Title 3'
+    option :a1, description: 'Ja'
+    option :a2, description: 'Nee'
+  end
+end
