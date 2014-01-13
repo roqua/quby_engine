a02d83929091a53aeba05ea876e3cc3eea1ce3f8
Ivan Malykh <ivan@roqua.nl>
2014-01-13 11:50:17 +0100

    Added spec that checks pagination advancement

--- spec/views/quby/answers/paged/_panel_spec.rb
+++ spec/views/quby/answers/paged/_panel_spec.rb
@@ -11,5 +11,22 @@ module Quby
       page.should have_selector(".step-3")
     end
 
+    it 'shows the user is on the next page' do
+      visit_new_answer_for(questionnaire)
+      @questionnaire = questionnaire
+
+      page.should have_selector("#panel0 .step-1.active")
+      page.should_not have_selector("#panel0 .step-2.active")
+      page.should_not have_selector("#panel0 .step-3.active")
+
+      page.should have_selector("#panel1 .step-1.active")
+      page.should have_selector("#panel1 .step-2.active")
+      page.should_not have_selector("#panel1 .step-3.active")
+
+      page.should have_selector("#panel2 .step-1.active")
+      page.should have_selector("#panel2 .step-2.active")
+      page.should have_selector("#panel2 .step-3.active")
+    end
+
   end
 end
