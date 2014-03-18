16f733f6b6432ada87079515a252650da2f28472
Jorn van de Beek <jornvandebeek@gmail.com>
2014-03-18 15:22:59 +0100

    Revert not actually updating watch library

--- vendor/assets/javascripts/jquery.watch.js
+++ vendor/assets/javascripts/jquery.watch.js
@@ -14,130 +14,129 @@
 
 (function($){
 
+  /**
+   * Watch Method
+   *
+   * @param (String) the name of the properties to watch
+   * @param (Object) options to overide defaults (only 'throttle' right now)
+   * @param (Function) callback function to be executed when attributes change
+   *
+   * @return (jQuery Object) returns the jQuery object for chainability
+   */
+  $.fn.watch = function(props, options, callback){
+
+    // Div element
+    var div = document.createElement('div');
+
+    // Check MutationObserver
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+
+    // CustomEvent with fallback
+    var CustomEvent = window.CustomEvent || function(){ return arguments || {}; };
+
     /**
-     * Watch Method
+     * Checks Support for Event
      *
-     * @param (String) the name of the properties to watch
-     * @param (Object) options to overide defaults (only 'throttle' right now)
-     * @param (Function) callback function to be executed when attributes change
+     * @param (String) the name of the event
+     * @param (Element Object) the element to test support against
      *
-     * @return (jQuery Object) returns the jQuery object for chainability
+     * @return {Boolean} returns result of test (true/false)
      */
-    $.fn.watch = function(props, options, callback){
+    var isEventSupported = function(eventName, el){
+      eventName = 'on' + eventName;
+      var supported = (eventName in el);
+      if(!supported){
+        el.setAttribute(eventName, 'return;');
+        supported = typeof el[eventName] == 'function';
+      }
+      return supported;
+    };
 
-        // Div element
-        var div = document.createElement('div');
+    // Type check options
+    if(typeof(options) == 'function'){
+      callback = options;
+      options = {};
+    }
 
-        // Check MutationObserver
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    // Type check callback
+    if(typeof(callback) != 'function')
+      callback = function(){};
 
-        // CustomEvent with fallback
-        var CustomEvent = window.CustomEvent || function(){ return arguments || {}; };
+    // Map options over defaults
+    options = $.extend({}, { throttle : 100 }, options);
 
-        /**
-         * Checks Support for Event
-         *
-         * @param (String) the name of the event
-         * @param (Element Object) the element to test support against
-         *
-         * @return {Boolean} returns result of test (true/false)
-         */
-        var isEventSupported = function(eventName, el){
-            eventName = 'on' + eventName;
-            var supported = (eventName in el);
-            if(!supported){
-                el.setAttribute(eventName, 'return;');
-                supported = typeof el[eventName] == 'function';
-            }
-            return supported;
-        };
+    /**
+     * Checks if properties have changed
+     *
+     * @param (Element Object) the element to watch
+     */
+    var check = function($el){
 
-        // Type check options
-        if(typeof(options) == 'function'){
-            callback = options;
-            options = {};
+      var that = this;
+
+      $.each(this.watching, function(){
+
+        // Setup
+        var data = this;
+        var changed = false;
+        var temp;
+
+        // Loop through properties
+        for(var i=0;i < data.props.length; i++){
+          temp = $el.prop(data.props[i]) || $el[0].attributes[data.props[i]] || $el.css(data.props[i]);
+          if(data.vals[i] != temp){
+            data.vals[i] = temp;
+            changed = true;
+            break;
+          }
         }
 
-        // Type check callback
-        if(typeof(callback) != 'function')
-            callback = function(){};
+        // Run callback if property has changed
+        if(changed && data.callback)
+          data.callback.call(that, new CustomEvent('AttrChange'));
 
-        // Map options over defaults
-        options = $.extend({}, { throttle : 10 }, options);
+      });
 
-        /**
-         * Checks if properties have changed
-         *
-         * @param (Element Object) the element to watch
-         */
-        var check = function($el){
-
-            var that = this;
-
-            $.each(this.watching, function(){
-
-                // Setup
-                var data = this;
-                var changed = false;
-                var temp;
-
-                // Loop through properties
-                for(var i=0;i < data.props.length; i++){
-                    temp = $el[0].attributes[data.props[i]] || $el.css(data.props[i]);
-                    if(data.vals[i] != temp){
-                        data.vals[i] = temp;
-                        changed = true;
-                        break;
-                    }
-                }
-
-                // Run callback if property has changed
-                if(changed && data.callback)
-                    data.callback.call(that, new CustomEvent('AttrChange'));
-
-            });
-
-        };
-
-        // Iterate over each element
-        return this.each(function(){
-
-            var that = this;
-            var $el = $(this);
-            var data = {
-                props: props.split(' '),
-                vals: [],
-                changed: [],
-                callback: callback
-            };
-
-            // Grab each properties initial value
-            $.each(data.props, function(i){
-                data.vals[i] = $el[0].attributes[data.props[i]] || $el.css(data.props[i]);
-                data.changed[i] = false;
-            });
-
-            // Set data
-            if(!this.watching)
-                this.watching = [];
-            this.watching.push(data);
-
-            // Choose method of watching and fallback
-            if(MutationObserver){
-                var observer = new MutationObserver(function(mutations){
-                    mutations.forEach(function(mutation){
-                        callback.call(that, mutation);
-                    });
-                });
-                observer.observe(this, { subtree: false, attributes: true });
-            } else if(isEventSupported('DOMAttrModified', div)){
-                $el.on('DOMAttrModified', callback);
-            } else if(isEventSupported('propertychange', div)){
-                $el.on('propertychange', callback);
-            } else {
-                setInterval(function(){ check.call(that, $el); }, options.throttle);
-            }
-        });
     };
+
+    // Iterate over each element
+    return this.each(function(){
+
+      var that = this;
+      var $el = $(this);
+      var data = {
+        props: props.split(' '),
+        vals: [],
+        changed: [],
+        callback: callback
+      };
+
+      // Grab each properties initial value
+      $.each(data.props, function(i){
+        data.vals[i] = $el.prop(data.props[i]) || $el[0].attributes[data.props[i]] || $el.css(data.props[i]);
+        data.changed[i] = false;
+      });
+
+      // Set data
+      if(!this.watching)
+        this.watching = [];
+      this.watching.push(data);
+
+      if (options['call_immediately'])
+        callback.call(that, $el)
+
+      // Choose method of watching and fallback
+      if(MutationObserver){
+        var observer = new MutationObserver(function(mutations){
+          mutations.forEach(function(e) {
+            check.call(that, $el);
+          });
+        });
+        observer.observe(that, { subtree: false, attributes: true });
+      } else {
+        setInterval(function(){ check.call(that, $el); }, options.throttle);
+      }
+    });
+  };
 
 })(jQuery);
