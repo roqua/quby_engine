027cc60978d49f90b0e67c31e30e78d1c2c9722d
Marten Veldthuis <marten@veldthuis.com>
2014-03-19 11:34:44 +0100

    Add some specs for AnswerRepos::MongoidRepo

--- spec/repos/quby/answer_repos/mongoid_repo_spec.rb
+++ spec/repos/quby/answer_repos/mongoid_repo_spec.rb
@@ -3,7 +3,28 @@ require 'spec_helper'
 module Quby
   module AnswerRepos
     describe MongoidRepo do
+      let(:repo) { MongoidRepo.new }
 
+      it 'creates new records' do
+        answer = repo.create!('big', value: {v_1: 'test'})
+        answer.value.should eq(v_1: 'test')
+      end
+
+      it 'finds records' do
+        answer    = repo.create!('big', value: {v_1: 'test'})
+        retrieved = repo.find('big', answer.id)
+        retrieved.id.should eq(answer.id)
+        retrieved.value.should eq('v_1' => 'test')
+      end
+
+      it 'updates records' do
+        answer    = repo.create!('big', value: {v_1: 'test'})
+        answer.value = {v_2: 'bar'}
+        repo.update!(answer)
+
+        retrieved = repo.find('big', answer.id)
+        retrieved.value.should eq("v_2" => "bar")
+      end
     end
   end
 end
