b9fb50f3692c2d1fb1d83495f944e1bd93d95de0
Henk <henk.van.der.veen@gmail.com>
2014-03-19 12:01:35 +0100

    removed SpaceBeforeBlockBraces from rubocop todo and fixed issues

--- spec/models/quby/items/panel_spec.rb
+++ spec/models/quby/items/panel_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Quby
   describe Items::Panel do
     it "should be possible to make without options" do
-      expect{ Quby::Items::Panel.new }.to_not raise_error
+      expect { Quby::Items::Panel.new }.to_not raise_error
     end
   end
 end
