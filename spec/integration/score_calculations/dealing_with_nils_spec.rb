47e9bc2e7aea28c060ada672b156593d2de43d53
Marten Veldthuis <marten@veldthuis.com>
2014-04-10 12:17:34 +0200

    Add integration tests for score calculations

---
+++ spec/integration/score_calculations/dealing_with_nils_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+module Quby
+  describe 'Score calculations' do
+    describe 'when dealing with nil values' do
+      it 'can be ignored, raising an error' do
+        score = Score.new :som, label: "Somscore" do
+          {value: sum(values(:v_1, :v_2, :v_3))}
+        end
+        expect { outcome(score, v_1: 3, v_2: 3, v_3: nil) }.to raise_error
+      end
+
+      it 'can be compacted' do
+        score = Score.new :som, label: "Somscore" do
+          {value: sum(values_with_nils(:v_1, :v_2, :v_3).compact)}
+        end
+        expect(outcome(score, v_1: 3, v_2: 3, v_3: nil)).to eq({value: 6})
+      end
+
+      it 'can be extrapolated when enough values are present' do
+        score = Score.new :som, label: "Somscore" do
+          {value: sum_extrapolate(values_with_nils(:v_1, :v_2, :v_3), 2)}
+        end
+        expect(outcome(score, v_1: 3, v_2: 3, v_3: nil)).to eq({value: 9})
+      end
+
+      it 'can be extrapolated when 80 percent is present' do
+        score = Score.new :som, label: "Somscore" do
+          {value: sum_extrapolate_80_pct(values_with_nils(:v_1, :v_2, :v_3, :v_4, :v_5))}
+        end
+        expect(outcome(score, v_1: 3, v_2: 3, v_3: 3, v_4: 3, v_5: nil)).to eq({value: 15})
+      end
+    end
+
+    def outcome(score, values = {})
+      ScoreCalculator.calculate(values.stringify_keys, Time.now, {}, {}, &score.calculation)
+    end
+  end
+end
