d1c5bebe72d77c5592acef37fff048c74cb83276
Ivan Malykh <ivan@roqua.nl>
2014-05-07 18:12:03 +0200

    updated opencpu gem version to 0.6.0 or higher
    Closes #72
    
    LGTM given by: @marten
    
    Squashed commit of the following:
    
    commit 07ad4f4bc410af12f65868567f15176cbe71ceea
    Author: Ivan Malykh <ivan@roqua.nl>
    Date:   Wed May 7 17:50:40 2014 +0200
    
        updated opencpu gem version to 0.6.0 or higher
    
    commit 7d29adde7de2aed051c77c574b4cafdff9cc1855
    Author: Ivan Malykh <ivan@roqua.nl>
    Date:   Wed May 7 17:50:18 2014 +0200
    
        removed unneeded config parameters

--- config/initializers/opencpu.rb
+++ config/initializers/opencpu.rb
@@ -1,6 +1,4 @@
 require 'opencpu'
 OpenCPU.configure do |config|
   config.endpoint_url = 'https://staging.opencpu.roqua.nl/ocpu'
-  config.username     = 'admin'
-  config.password     = 'password'
 end
