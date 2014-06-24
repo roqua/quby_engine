766a62bd0b01c87a9f4a5620cf96a84fd231f20a
Marten Veldthuis <marten@veldthuis.com>
2014-06-24 14:27:23 +0200

    Fix spec run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,8 @@ Capybara.register_driver :poltergeist do |app|
 end
 Capybara.javascript_driver = :poltergeist
 
+Quby.questionnaires_path = Rails.root.join("..", "..", "spec", "fixtures")
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("../../spec/support/**/*.rb")].each { |f| require f }
