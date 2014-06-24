2a64c7a81c9fddee16557cd8177c98570aaeb00c
Samuel Esposito <s.esposito@roqua.nl>
2014-06-24 15:03:01 +0200

    extend answers api specs

---
+++ spec/fixtures/simple.rb
@@ -0,0 +1,15 @@
+title 'SimpleQuestionnaire'
+
+panel do
+  title 'A simple questionnaire'
+
+  question :v_1, type: :radio do
+    title 'A simple question'
+    description 'A simple instruction'
+    option :a1, value: 0, description: 'Simple option 1'
+    option :a2, value: 1, description: 'Simple option 2'
+    option :a3, value: 2, description: 'Simple option 3'
+    option :a4, value: 3, description: 'Simple option 4'
+    option :a5, value: 4, description: 'Simple option 5'
+  end
+end
