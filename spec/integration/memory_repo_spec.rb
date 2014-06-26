210be3c10840ef733699cdd506f2e1d50d712ccf
Marten Veldthuis <marten@veldthuis.com>
2014-06-26 21:52:14 +0200

    centralize answer specs

--- spec/integration/memory_repo_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-module Quby::Answers::Repos
-  describe MemoryRepo do
-    before { Quby.answer_repo = MemoryRepo.new }
-
-    it_behaves_like 'a valid backend for the answers api'
-  end
-end
--- spec/repos/quby/answer_repos/memory_repo_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-module Quby::Answers::Repos
-  describe MemoryRepo do
-    it_behaves_like "an answer repository"
-  end
-end
