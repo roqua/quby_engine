3eb10c94975732d3a4d88300886e195759f1f356
Marten Veldthuis <marten@veldthuis.com>
2014-06-26 14:37:06 +0200

    Add #build to DSL

--- app/dsl/quby/questionnaire_dsl.rb
+++ app/dsl/quby/questionnaire_dsl.rb
@@ -1,6 +1,14 @@
 require 'quby/questionnaire_dsl/questionnaire_builder'
 module Quby
   module QuestionnaireDsl
+    def self.build(key, definition = nil, &block)
+      Questionnaire.new(key).tap do |questionnaire|
+        builder = QuestionnaireBuilder.new(questionnaire)
+        builder.instance_eval(definition) if definition
+        builder.instance_eval(block) if block
+      end
+    end
+
     def self.enhance(target_instance, definition)
       q = QuestionnaireBuilder.new(target_instance)
       q.instance_eval(definition)
