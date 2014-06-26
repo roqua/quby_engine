35ecbce733a6e3c148c7d45849f667d9e332e786
Marten Veldthuis <marten@veldthuis.com>
2014-06-26 16:24:02 +0200

    Fix for Rails 4 autoloading changes

--- lib/quby.rb
+++ lib/quby.rb
@@ -6,6 +6,7 @@ $:.unshift(File.expand_path("../../app/models", __FILE__))
 $:.unshift(File.expand_path("../../app/repos", __FILE__))
 $:.unshift(File.expand_path("../../app/services", __FILE__))
 
+require 'quby/dsl'
 require 'quby/questionnaire_repos/disk_repo'
 require 'quby/answer_repos'
 require 'quby/outcome_calculation'
