3fe68de03f2bcef61a73c46a97c1ac69f39c3bba
Marten Veldthuis <marten@veldthuis.com>
2014-07-03 13:41:05 +0200

    Add caching to API

--- lib/quby/questionnaires/api.rb
+++ lib/quby/questionnaires/api.rb
@@ -3,11 +3,14 @@ module Quby
     class API
       def initialize(questionnaire_repo:)
         @repo = questionnaire_repo
+        @cache = {}
       end
 
       def find(key)
+        return @cache[key][:questionnaire] if fresh?(key)
         definition = @repo.find key
-        DSL.build_from_definition(definition)
+        @cache[key] = {questionnaire: DSL.build_from_definition(definition), timestamp: definition.timestamp}
+        @cache[key][:questionnaire]
       end
 
       def exists?(questionnaire_key)
@@ -22,6 +25,13 @@ module Quby
         definition = Entities::Definition.new(key: key, sourcecode: sourcecode)
         definition.valid?
         definition
+      end
+
+      private
+
+      def fresh?(key)
+        return false unless @cache.key?(key)
+        @cache[key][:timestamp].to_i == @repo.timestamp(key)
       end
     end
   end
