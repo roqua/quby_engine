0d908541188cf160b9c5dcdcd3f6f32b619d71df
Marten Veldthuis <marten@veldthuis.com>
2014-07-11 13:01:27 +0200

    Reflow comment blocks

--- lib/quby/questionnaires/entities/fields.rb
+++ lib/quby/questionnaires/entities/fields.rb
@@ -6,15 +6,15 @@ module Quby
       class Fields
         attr_reader :question_hash
 
-        # An +answer_key+ is a key that will exist in the values hash of an answer. This means that
-        # answer keys for radio's will be just the question key, and answer keys for checkboxes will
-        # be the keys of all the options. These are the POST parameters when submitting the form,
-        # an so they must be globally unique or we won't know which question the received data belongs to.
+        # An +answer_key+ is a key that will exist in the values hash of an answer. This means that answer keys for
+        # radio's will be just the question key, and answer keys for checkboxes will be the keys of all the options.
+        # These are the POST parameters when submitting the form, an so they must be globally unique or we won't know
+        # which question the received data belongs to.
         attr_reader :answer_keys
 
-        # An +input_key+ is a key that uniquely identifies a single <input> tag. For radio's, every
-        # radio option will have it's own input key. This is needed because option keys must
-        # be globally unique so that they can be targeted by :depends_on relations.
+        # An +input_key+ is a key that uniquely identifies a single <input> tag. For radio's, every radio option will
+        # have it's own input key. This is needed because option keys must be globally uniqueso that they can be
+        # targeted by :depends_on relations.
         attr_reader :input_keys
 
         def initialize
@@ -44,10 +44,9 @@ module Quby
           @question_hash.key?(key) || @input_keys.include?(key)
         end
 
-        # Given a list of question and option keys returns a list of input-keys.
-        # If a given key is a question-key, adds the question.input_keys
-        # If a given key is an option-input-key it adds the given key.
-        # Raises an error if a key is not defined.
+        # Given a list of question and option keys returns a list of input-keys. If a given key is a question-key,
+        # adds the question.input_keys If a given key is an option-input-key it adds the given key. Raises an error
+        # if a key is not defined.
         def expand_input_keys(keys)
           keys.reduce([]) do |ikeys, key|
             if question_hash.key?(key)
