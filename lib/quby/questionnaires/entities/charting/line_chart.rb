4ec065c35a1d45360c664b4d5ca9e461b91fef1a
Henk <henk.van.der.veen@gmail.com>
2014-07-28 16:12:04 +0200

    Added range and tick_interval to all charts. Removed unused stepsize from linecharts.

--- lib/quby/questionnaires/entities/charting/line_chart.rb
+++ lib/quby/questionnaires/entities/charting/line_chart.rb
@@ -6,8 +6,6 @@ module Quby
       module Charting
         class LineChart < Chart
           attribute :y_label,                    String
-          attribute :y_range,                    Range
-          attribute :y_stepsize,                 Float
           attribute :tonality,                   Symbol, default: :lower_is_better
           attribute :baseline,                   Proc
           attribute :clinically_relevant_change, Float
