bf49e38c7a0fb6256e4a2bf711e00361c886e7b9
Henk <henk.van.der.veen@gmail.com>
2014-09-15 15:10:58 +0200

    Note: Waar {{var}} eerst naar
        <span..>{{var}}</span>
    
    ging, gaat hij nu naar
    
        <span..>{var}</span>
    
    Omdat het anders 2x recursief vervangen wordt.
    
    Closes #95
    
    LGTM given by: @ivdma
    
    Squashed commit of the following:
    
    commit 70d4ff20afbf347ff400b83fd8e727f0fff03f4b
    Author: Henk <henk.van.der.veen@gmail.com>
    Date:   Mon Sep 15 10:15:57 2014 +0200
    
        updated maruku, fixed maruku extension, fixed specs

--- spec/quby/extensions/maruku_extensions_spec.rb
+++ spec/quby/extensions/maruku_extensions_spec.rb
@@ -5,17 +5,17 @@ class Maruku
 
   describe "link tag extenstion ~~url~~body~~" do
     it "converts ~~url~~body~~ into a modal frame link" do
-      Maruku.new("~~http://google.com~~google~~").to_html.should == "<p><a href='#' onclick='modalFrame(\"http://google.com\");'>google</a></p>"
+      Maruku.new("~~http://google.com~~google~~").to_html.strip.should == "<p><a href='#' onclick='modalFrame(\"http://google.com\");'>google</a></p>"
     end
   end
 
   describe "text variable extenstion {{text_var}}" do
     it "converts {{test}} into a text variable span" do
-      Maruku.new("{{test}}").to_html.should == "<p><span class='text_var' text_var='test'>{{test}}</span></p>"
+      Maruku.new("{{test}}").to_html.strip.should == "<p><span class='text_var' text_var='test'>{test}</span></p>"
     end
 
     it "properly converts 2 text tags next to each other" do
-      Maruku.new("{{test}} {{test2}}").to_html.should == "<p><span class='text_var' text_var='test'>{{test}}</span> <span class='text_var' text_var='test2'>{{test2}}</span></p>"
+      Maruku.new("{{test}} {{test2}}").to_html.strip.should == "<p><span class='text_var' text_var='test'>{test}</span> <span class='text_var' text_var='test2'>{test2}</span></p>"
     end
   end
 
