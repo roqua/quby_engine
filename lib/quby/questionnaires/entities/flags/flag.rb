5a8417c669c0f068d4a46e98ef8b4450bb7143ec
Jorn van de Beek <jornvandebeek@gmail.com>
2014-09-19 15:17:46 +0200

    Add descriptions for true and false to flag. Properly require flag file

--- lib/quby/questionnaires/entities/flags/flag.rb
+++ lib/quby/questionnaires/entities/flags/flag.rb
@@ -1,7 +1,8 @@
 module Quby
   module Questionnaires
     module Entities
-      class Flag < Struct.new(:key, :description_true, :description_false, :shows_questions, :hides_questions)
+      class Flag < Struct.new(:key, :description_true, :description_false,
+                              :shows_questions, :hides_questions)
         def initialize(key:, description_true:, description_false:, shows_questions: [], hides_questions: [])
           super(key, description_true, description_false, shows_questions, hides_questions)
         end
