a686e69ef8e7a3fb77bb10916c57b1cd36f38324
Jorn van de Beek <jornvandebeek@gmail.com>
2014-10-01 14:43:23 +0200

    Add validation of shows/hides_questions keys for flags

--- lib/quby/questionnaires/entities/flag.rb
+++ lib/quby/questionnaires/entities/flag.rb
@@ -1,16 +1,17 @@
 module Quby
   module Questionnaires
     module Entities
-      class Flag < Struct.new(:key, :description_true, :description_false, :internal,
+      class Flag < Struct.new(:key, :description_true, :description_false, :internal, :trigger_on,
                               :shows_questions, :hides_questions)
         # rubocop:disable ParameterLists
         def initialize(key:,
                        description_true:,
                        description_false:,
                        internal: false,
+                       trigger_on: true,
                        shows_questions: [],
                        hides_questions: [])
-          super(key, description_true, description_false, internal, shows_questions, hides_questions)
+          super(key, description_true, description_false, internal, trigger_on, shows_questions, hides_questions)
         end
         # rubocop:enable ParameterLists
       end
