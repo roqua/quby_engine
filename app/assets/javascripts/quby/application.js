32583adc849b289757520b28648cfda1bc3a1ca3
Marten Veldthuis <marten@veldthuis.com>
2014-10-13 18:57:13 +0200

    Convert textvars to react

--- app/assets/javascripts/quby/application.js
+++ app/assets/javascripts/quby/application.js
@@ -14,6 +14,12 @@
 //= require jquery_placeholder
 //= require jquery.watch.js
 //= require jquery.nouislider.with_roqua_modifications.js
+
+//= require es5-shim.min.js
+//= require es5-sham.min.js
+//= require html5shiv-printshiv.min.js
+//= require react-with-addons-0.11-ie7-patched.js
+
 //= require ./better_range_inputs.js
 //= require jquery.ba-hashchange.js
 //= require jquery.deselectableRadio.1.0.3.js
