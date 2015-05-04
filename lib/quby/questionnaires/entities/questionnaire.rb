0bc3a8adc7d8fd1e7aef517e2b6a2ce5efa44da7
Marten Veldthuis <marten@veldthuis.com>
2015-05-04 13:46:27 +0200

    Improve answer initialisation speed

--- lib/quby/questionnaires/entities/questionnaire.rb
+++ lib/quby/questionnaires/entities/questionnaire.rb
@@ -236,6 +236,114 @@ module Quby
                   .map    { |key, textvar| [key, textvar.default] }
                   .to_h
         end
+
+        def answer_dsl_module
+          # Have to put this in a local variable so the module definition block can access it
+          questions_in_var = questions
+
+          @answer_dsl_cache ||= Module.new do
+            questions_in_var.each do |question|
+              next if question.andand.key.blank?
+              case question.type
+              when :date
+
+                define_method(question.year_key) do
+                  self.value ||= Hash.new
+                  self.value[question.year_key]
+                end
+
+                define_method("#{question.year_key}=") do |v|
+                  self.value ||= Hash.new
+                  self.value[question.year_key] = v
+                end
+
+                define_method(question.month_key) do
+                  self.value ||= Hash.new
+                  self.value[question.month_key]
+                end
+
+                define_method("#{question.month_key}=") do |v|
+                  self.value ||= Hash.new
+                  self.value[question.month_key] = v
+                end
+
+                define_method(question.day_key) do
+                  self.value ||= Hash.new
+                  self.value[question.day_key]
+                end
+
+                define_method("#{question.day_key}=") do |v|
+                  self.value ||= Hash.new
+                  self.value[question.day_key] = v
+                end
+
+                define_method(question.key) do
+                  self.value ||= Hash.new
+                  v = [self.value[question.day_key.to_s   || "#{question.key}_dd"],
+                       self.value[question.month_key.to_s || "#{question.key}_mm"],
+                       self.value[question.year_key.to_s  || "#{question.key}_yyyy"]]
+                  if v.reduce(true) { |allblank, it| it.blank? and allblank }
+                    return ""
+                  else
+                    return v.join("-")
+                  end
+                end
+
+              when :check_box
+
+                define_method(question.key) do
+                  self.value ||= Hash.new
+                  self.value[question.key.to_s] ||= Hash.new
+                end
+
+                question.options.each do |opt|
+                  next if opt.andand.key.blank?
+                  define_method("#{opt.key}") do
+                    self.value ||= Hash.new
+                    self.value[question.key.to_s] ||= Hash.new
+                    self.value[opt.key.to_s] ||= 0
+                  end
+
+                  define_method("#{opt.key}=") do |v|
+                    v = v.to_i
+                    self.value ||= Hash.new
+                    self.value[question.key.to_s] ||= Hash.new
+                    self.value[question.key.to_s][opt.key.to_s] = v
+                    self.value[opt.key.to_s] = v
+                  end
+                end
+              else
+                # Includes:
+                # question.type == :radio
+                # question.type == :scale
+                # question.type == :select
+                # question.type == :string
+                # question.type == :textarea
+                # question.type == :integer
+                # question.type == :float
+
+                if [:radio, :scale, :select].include? question.type # getters for individual question options
+                  question.options.each do |opt|
+                    define_method("#{question.key}_#{opt.key}") do
+                      self.value ||= Hash.new
+                      self.value[question.key.to_s] == opt.key.to_s
+                    end
+                  end
+                end
+
+                define_method(question.key) do
+                  self.value ||= Hash.new
+                  self.value[question.key.to_s]
+                end
+
+                define_method(question.key.to_s + "=") do |v|
+                  self.value ||= Hash.new
+                  self.value[question.key.to_s] = v
+                end
+              end rescue nil
+            end
+          end
+        end
       end
     end
   end
