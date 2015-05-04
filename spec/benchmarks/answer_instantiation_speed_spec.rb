1c4ceb1a8cf03f48234d7b670c221334a7fdce74
Marten Veldthuis <marten@veldthuis.com>
2015-05-04 17:48:06 +0200

    Optimize Quby answer initialization and add preload support
    * Add support for additional parameters to the  method (this allows RoQua's answer repo to supply a preloaded record object, which avoids N+1 queries.
    
    * Move from  to a dynamically defined and cached module with which the answer object is extended. This is an order of magnitude faster (from 10sec to 0.7sec for the supplied benchmark).
    
    Closes #119
    
    LGTM given by: @jornvandebeek
    
    Squashed commit of the following:
    
    commit 0bc3a8adc7d8fd1e7aef517e2b6a2ce5efa44da7
    Author: Marten Veldthuis <marten@veldthuis.com>
    Date:   Mon May 4 13:46:27 2015 +0200
    
        Improve answer initialisation speed
    
    commit 026cc88d1d3be4bf0bd49e53e29173f787e8ab77
    Author: Marten Veldthuis <marten@veldthuis.com>
    Date:   Mon May 4 12:12:52 2015 +0200
    
        Add support for additional parameters to the answer repo

---
+++ spec/benchmarks/answer_instantiation_speed_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'benchmark'
+require 'flamegraph'
+
+describe 'Benchmark making answer entities', benchmark: true do
+  let(:definition) do
+    pieces = []
+
+    500.times do |i|
+      pieces << <<-END
+        question :v_#{i}, type: :radio, required: false do
+          title "Choose"
+          option :a1, value: 1, description: "Ja"
+          option :a2, value: 2, description: "Nee"
+        end
+      END
+    end
+
+    pieces.join("\n\n")
+
+  end
+
+  let(:questionnaire) { inject_questionnaire("test", definition) }
+  let!(:sample_count) { 1000 }
+
+  it 'is quick for large questionnaires' do
+    # initialize questionnaire outside profiler
+    RubyProf.pause if RubyProf.running?
+    questionnaire.questions
+    expect(questionnaire.key).to eq('test')
+    Quby::Answers::Entities::Answer.new(questionnaire_key: questionnaire.key).enhance_by_dsl
+    RubyProf.resume if RubyProf.running?
+
+    sample_count.times do
+      Quby::Answers::Entities::Answer.new(questionnaire_key: questionnaire.key).enhance_by_dsl
+    end
+  end
+end
