bae680303897b2e140709eb6dae3a9c5e1f237eb
Henk <henk.van.der.veen@gmail.com>
2016-05-10 14:09:01 +0200

    use fail, not raise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ Combustion.initialize! :action_controller, :action_view, :sprockets do
   # config.action_view.raise_on_missing_translations = true # only works for rails > 4.1
 end
 I18n.exception_handler = lambda do |exception, locale, key, options|
-  raise "missing translation: #{key}"
+  fail "missing translation: #{key}"
 end
 
 require 'rspec/rails'
