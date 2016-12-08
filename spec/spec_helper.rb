9ed5651b6c571814acaf77f78c1327cca1418326
Daan Davidsz <daan@roqua.nl>
2016-12-08 11:46:15 +0100

    Add config for CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@ require 'rails'
 require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
-require 'jquery/rails'
 require 'combustion'
 Combustion.path = 'spec/internal'
 Combustion.initialize! :action_controller, :action_view, :sprockets do
