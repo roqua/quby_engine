126dad9211ba9a041c0beab515db5a0e7cd156b8
Daan Davidsz <daan@roqua.nl>
2016-12-13 14:20:55 +0100

    Use new style hash syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,9 +58,9 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   [:controller, :view, :request].each do |type|
-    config.include ::Rails::Controller::Testing::TestProcess, :type => type
-    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
-    config.include ::Rails::Controller::Testing::Integration, :type => type
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
   end
 
   config.before(:each) do
