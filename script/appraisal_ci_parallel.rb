509baabc60bfb676370c917fbbd1f68b4830546d
Daan Davidsz <daan@roqua.nl>
2016-12-13 14:10:52 +0100

    Only run Rails 5 specs for now

--- script/appraisal_ci_parallel.rb
+++ script/appraisal_ci_parallel.rb
@@ -7,12 +7,6 @@
 
 case ENV["CIRCLE_NODE_INDEX"]
 when "0"
-  puts "Running #{ARGV.join(" ")} for Rails 4.1"
-  exec({"BUNDLE_GEMFILE" => "gemfiles/rails41.gemfile"}, ARGV.join(" "))
-when "1"
-  puts "Running #{ARGV.join(" ")} for Rails 4.2"
-  exec({"BUNDLE_GEMFILE" => "gemfiles/rails42.gemfile"}, ARGV.join(" "))
-when "2"
   puts "Running #{ARGV.join(" ")} for Rails 5"
   exec({"BUNDLE_GEMFILE" => "gemfiles/rails5.gemfile"}, ARGV.join(" "))
 end
