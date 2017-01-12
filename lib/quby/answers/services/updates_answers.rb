ea1e0dea3e9ef3c6c6e223be61216af8d04ceb90
Jorn van de Beek <jornvandebeek@gmail.com>
2017-01-12 16:43:17 +0100

    Split off preserving raw_params into its own method

--- lib/quby/answers/services/updates_answers.rb
+++ lib/quby/answers/services/updates_answers.rb
@@ -12,35 +12,28 @@ module Quby
         end
 
         def update(new_attributes = {})
-          answer.raw_params = new_attributes
-          backup_answer = answer.clone
+          save_raw_params_on_error(new_attributes) do
+            attribute_filter = FiltersAnswerValue.new(answer.questionnaire)
+            attribute_filter.filter(new_attributes).each { |name, value| answer.send("#{name}=", value) }
 
-          attribute_filter   = FiltersAnswerValue.new(answer.questionnaire)
-          attribute_filter.filter(new_attributes).each { |name, value| answer.send("#{name}=", value) }
+            answer.extend AnswerValidations
+            answer.cleanup_input
+            answer.validate_answers
 
-          answer.extend AnswerValidations
-          answer.cleanup_input
-          answer.validate_answers
-
-          if answer.errors.empty?
-            if new_attributes["rendered_at"]
-              started_at = Time.at(new_attributes["rendered_at"].to_i)
+            if answer.errors.empty?
+              if new_attributes["rendered_at"]
+                started_at = Time.at(new_attributes["rendered_at"].to_i)
+              else
+                started_at = nil
+              end
+              answer.mark_completed(started_at)
+              answer.outcome = OutcomeCalculation.new(answer).calculate
+              Quby.answers.update!(answer)
+              succeed!
             else
-              started_at = nil
+              fail!
             end
-            answer.mark_completed(started_at)
-            answer.outcome = OutcomeCalculation.new(answer).calculate
-            Quby.answers.update!(answer)
-            succeed!
-          else
-            fail!
           end
-        rescue
-          # we dont save answer since it probably has a weird state now, instead we save the a clone of answer
-          # that just has the raw_params set on it, so we can do data recovery with the raw_params
-          backup_answer.raw_params['could_not_update_at'] = DateTime.now.to_i # to note this is was a failed update
-          Quby.answers.update!(backup_answer)
-          raise
         end
 
         def on_success(&block)
@@ -52,6 +45,20 @@ module Quby
         end
 
         protected
+
+        def save_raw_params_on_error(new_attributes, &_block)
+          answer.raw_params = new_attributes
+          backup_answer = answer.clone
+
+          yield
+
+        rescue
+          # we dont save answer since it probably has a weird state now, instead we save the a clone of answer
+          # that just has the raw_params set on it, so we can do data recovery with the raw_params
+          backup_answer.raw_params['could_not_update_at'] = DateTime.now.to_i # to note this is was a failed update
+          Quby.answers.update!(backup_answer)
+          raise
+        end
 
         def succeed!
           @success_callback.call if @success_callback
