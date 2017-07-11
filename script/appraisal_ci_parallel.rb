b3fb8b61eec72b2c6d4415da4faed77a62fdd372
Jorn van de Beek <jornvandebeek@gmail.com>
2017-07-11 11:49:30 +0200

    Drop rails 4.2

--- script/appraisal_ci_parallel.rb
+++ script/appraisal_ci_parallel.rb
@@ -1,15 +1,4 @@
 #!/usr/bin/env ruby
 
-unless ENV["CIRCLE_NODE_TOTAL"] == "2"
- puts "Circle parallelism should be 2 (number of different Rails versions)"
- exit 1
-end
-
-case ENV["CIRCLE_NODE_INDEX"]
-when "0"
-  puts "Running #{ARGV.join(" ")} for Rails 4.2"
-  exec({"BUNDLE_GEMFILE" => "gemfiles/rails42.gemfile"}, ARGV.join(" "))
-when "1"
-  puts "Running #{ARGV.join(" ")} for Rails 5"
-  exec({"BUNDLE_GEMFILE" => "gemfiles/rails50.gemfile"}, ARGV.join(" "))
-end
+puts "Running #{ARGV.join(" ")} for Rails 5"
+exec({"BUNDLE_GEMFILE" => "gemfiles/rails50.gemfile"}, ARGV.join(" "))
