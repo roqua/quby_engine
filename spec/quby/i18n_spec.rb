a70a1cdd264652c23ac5bf7d74e0cdded567728b
Jorn van de Beek <jornvandebeek@gmail.com>
2017-11-08 12:10:52 +0100

    Use yield in i18n spec

--- spec/quby/i18n_spec.rb
+++ spec/quby/i18n_spec.rb
@@ -1,32 +1,30 @@
 require 'spec_helper'
 describe I18n do
   it 'has a translation in every other locale for every key in the nl locale' do
-    def to_key_array(hash, current_key = [], current_array = [])
-      hash.each do |k, v|
-        new_current_key = current_key + [k]
-        if v.is_a? Hash
-          to_key_array(v, new_current_key, current_array)
-        else
-          current_array << new_current_key
-        end
-      end
-
-      current_array
-    end
-
-    keys_in_nl_locale = to_key_array I18n.t('.', locale: :nl)
     errors = []
     (I18n.config.available_locales - [:nl]).each do |locale|
-      keys_in_nl_locale.each do |deep_key|
+      each_key_path(I18n.t('.', locale: :nl)) do |key_path|
         result = begin
-          I18n.t('.', locale: locale).dig(*deep_key)
+          I18n.t('.', locale: locale).dig(*key_path)
         rescue
           nil
         end
-        errors << "#{deep_key.inspect} is missing from locale :#{locale}" if result.blank?
+        errors << "#{key_path.inspect} is missing from locale :#{locale}" if result.blank?
       end
     end
 
     expect(errors).to eq([])
+  end
+
+  # yields for each leaf key `[:path, :to, :leaf]`
+  def each_key_path(hash, base_key = [], &block)
+    hash.each do |k, v|
+      current_key = base_key + [k]
+      if v.is_a? Hash
+        each_key_path(v, current_key, &block)
+      else
+        yield current_key
+      end
+    end
   end
 end
