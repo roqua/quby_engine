f7c056d532d8ff785eb4dc408a1abc273b5b634a
Jorn van de Beek <jornvandebeek@gmail.com>
2018-09-03 16:40:27 +0200

    Simplify condition

--- lib/active_model_modules/array_attribute_valid_validator.rb
+++ lib/active_model_modules/array_attribute_valid_validator.rb
@@ -2,12 +2,10 @@ class ArrayAttributeValidValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
     value.each.with_index do |element, index|
-      if element.respond_to?(:valid?)
-        unless element.valid?
-          record.errors.add(attribute, "element ##{index} #{element.errors.full_messages.join(', ')}")
-        end
-      else
+      if !element.respond_to?(:valid?)
         record.errors.add(attribute, "element ##{index} does not respond_to valid?")
+      elsif !element.valid?
+        record.errors.add(attribute, "element ##{index} #{element.errors.full_messages.join(', ')}")
       end
     end
   end
