5084a2d580a79dd4da1066b0541dddc8d386dc56
Jorn van de Beek <jornvandebeek@gmail.com>
2018-10-30 16:27:17 +0100

    Use ampersand

--- lib/active_model_modules/array_attribute_valid_validator.rb
+++ lib/active_model_modules/array_attribute_valid_validator.rb
@@ -1,7 +1,6 @@
 class ArrayAttributeValidValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value.blank?
-    value.each.with_index do |element, index|
+    value&.each&.with_index do |element, index|
       if !element.respond_to?(:valid?)
         record.errors.add(attribute, "element ##{index} does not respond_to valid?")
       elsif !element.valid?
