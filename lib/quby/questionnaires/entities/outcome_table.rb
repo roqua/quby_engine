e2ebcef55e9aeca86540ebbd180d58a4f171d15a
Jorn van de Beek <jornvandebeek@gmail.com>
2019-02-01 15:03:18 +0100

    Use proc instead of Proc.new

--- lib/quby/questionnaires/entities/outcome_table.rb
+++ lib/quby/questionnaires/entities/outcome_table.rb
@@ -6,7 +6,7 @@ module Quby
         attr_accessor :score_keys, :subscore_keys, :name, :default_collapsed, :questionnaire
 
         validates :score_keys, :subscore_keys, presence: true
-        validates :name, presence: true, if: Proc.new { |table| table.default_collapsed }
+        validates :name, presence: true, if: proc { |table| table.default_collapsed }
         validate :references_existing_score_keys
 
         def references_existing_score_keys
