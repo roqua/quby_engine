96d3ac65d3da66966c6784925fbb761741cc7edc
Marten Veldthuis <marten@veldthuis.com>
2019-08-13 09:38:43 +0200

    Test other cases

--- spec/features/allow_switching_to_bulk_spec.rb
+++ spec/features/allow_switching_to_bulk_spec.rb
@@ -3,10 +3,8 @@
 require 'spec_helper'
 
 feature 'Switching to bulk view' do
-  let(:questionnaire) do
-    inject_questionnaire("test", <<-END)
-      allow_switch_to_bulk
-
+  let(:panels) do
+    <<-END
       panel do
         text 'This will not appear in bulk'
 
@@ -27,11 +25,38 @@ feature 'Switching to bulk view' do
     END
   end
 
+  let(:allow_switch_questionnaire) do
+    inject_questionnaire("test", <<-END)
+      allow_switch_to_bulk
+      #{panels}
+    END
+  end
+
+  let(:disallow_switch_questionnaire) do
+    inject_questionnaire("test", <<-END)
+      allow_switch_to_bulk false
+      #{panels}
+    END
+  end
+
   scenario 'switch to bulk view', js: true do
-    visit_new_answer_for(questionnaire)
+    visit_new_answer_for(allow_switch_questionnaire)
     expect(page).to have_content('This will not appear in bulk')
     click_link "Bulk-weergave"
     expect(page).not_to have_content('This will not appear in bulk')
+  end
+
+  scenario 'switch only appears on the first page', js: true do
+    visit_new_answer_for(allow_switch_questionnaire)
+    expect(page).to have_content('This will not appear in bulk')
+    click_on 'Verder'
+    expect(page).not_to have_content('Bulk-weergave')
+  end
+
+  scenario 'disallowed switching to bulk view', js: true do
+    visit_new_answer_for(disallow_switch_questionnaire)
+    expect(page).to have_content('This will not appear in bulk')
+    expect(page).not_to have_content('Bulk-weergave')
   end
 end
 
