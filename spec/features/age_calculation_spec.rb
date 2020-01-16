91a18383c726a2895cdda1c36e343010ac417ad2
Marten Veldthuis <marten@roqua.nl>
2020-01-16 13:10:02 +0000

    Update wording of end panel to account for download PDF button

--- spec/features/age_calculation_spec.rb
+++ spec/features/age_calculation_spec.rb
@@ -5,7 +5,7 @@ module Quby
   describe 'Age in scores' do
     it 'can access age in a real score calculation' do
       answer  = Quby.answers.create!('aged_score_interpretations',
-                                     patient: {"birthyear" => 2000, "gender" => :male},
+                                     patient: {"birthyear" => Time.zone.now.year - 18, "gender" => :male},
                                      completed_at: Time.now)
 
       outcome = Quby.answers.generate_outcome(answer)
