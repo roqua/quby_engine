09fbf292c3dbf74e337ece9714eb41bec9b37196
Marten Veldthuis <marten@veldthuis.com>
2020-10-28 09:09:16 +0100

    Add lookup_tables to Compiler call

--- lib/quby/questionnaires/dsl.rb
+++ lib/quby/questionnaires/dsl.rb
@@ -3,14 +3,16 @@
 module Quby
   module Questionnaires
     module DSL
+      # Deprecated, precompile elsewhere and use from_json
       def self.build_from_definition(definition)
-        compiled = Quby::Compiler.compile(definition.key, definition.sourcecode, seeds: [], path: definition.path)
+        compiled = Quby::Compiler.compile(definition.key, definition.sourcecode, seeds: [], lookup_tables: {}, path: definition.path)
         data = JSON.parse(compiled[:outputs][:quby_frontend_v1].content)
         Deserializer.from_json(data)
       end
 
+      # Deprecated, precompile elsewhere and use from_json
       def self.build(key, sourcecode = nil, timestamp: nil, &block)
-        compiled = Quby::Compiler.compile(key, sourcecode, seeds: [], &block)
+        compiled = Quby::Compiler.compile(key, sourcecode, seeds: [], lookup_tables: {}, &block)
         data = JSON.parse(compiled[:outputs][:quby_frontend_v1].content)
         Deserializer.from_json(data)
       end
